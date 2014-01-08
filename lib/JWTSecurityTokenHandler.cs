29faf8cb2ba91e1101b4bea2f132ff426acb57a7
Brent Schmaltz <brentschmaltz@hotmail.com>
2014-01-08 07:56:03 -0800

    Updated Header

--- lib/JWTSecurityTokenHandler.cs
+++ lib/JWTSecurityTokenHandler.cs
@@ -1,6 +1,8 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="JwtSecurityTokenHandler.cs" company="Microsoft">Copyright 2012 Microsoft Corporation</copyright>
-// <license>
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved
+// Apache License 2.0
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -12,7 +14,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// </license>
+//-----------------------------------------------------------------------
 
 namespace System.IdentityModel.Tokens
 {
