76249099d820b2f469d3b5269369c4a650dda3a1
Brent Schmaltz <brentschmaltz@hotmail.com>
2014-01-14 13:48:26 -0800

    Removed null check in constructor, removed un-used file

---
+++ lib/X509SecurityKey.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.IdentityModel.Tokens;
+using System.Security.Cryptography.X509Certificates;
+
+namespace System.IdentityModel.Tokens
+{
+    /// <summary>
+    /// Security key that allows access to cert
+    /// </summary>
+    public class X509SecurityKey : X509AsymmetricSecurityKey
+    {
+        X509Certificate2 _certificate;
+        /// <summary>
+        /// Instantiates a <see cref="SecurityKey"/> using a <see cref="X509Certificate2"/>
+        /// </summary>
+        /// <param name="certificate"> cert to use.</param>
+        public X509SecurityKey(X509Certificate2 certificate)
+            : base(certificate)
+        {
+            _certificate = certificate;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="X509Certificate2"/>.
+        /// </summary>
+        public X509Certificate2 Certificate { get { return _certificate; } }
+    }
+}
