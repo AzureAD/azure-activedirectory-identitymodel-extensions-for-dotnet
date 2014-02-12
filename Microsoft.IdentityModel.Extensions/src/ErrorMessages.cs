ef0cec91f791975c301748e2b2a5a49cf9a4fe57
Brent Schmaltz <brentschmaltz@hotmail.com>
2014-02-12 11:39:22 -0800

    mods for wilson samlsecuritytokenhandler

--- Microsoft.IdentityModel.Extensions/src/ErrorMessages.cs
+++ Microsoft.IdentityModel.Extensions/src/ErrorMessages.cs
@@ -36,12 +36,13 @@ namespace Microsoft.IdentityModel.Extensions
         // messages pertaining to validation 10200 - 10299
         public const string IDX10200 = "IDX10200: Support for ValidateToken(string, TokenValidationParameters) requires a handler to implement ISecurityTokenValidator, none of the SecurityTokenHandlers did.";
         public const string IDX10201 = "IDX10201: None of the the SecurityTokenHandlers could read the 'securityToken': '{0}'.";
-        public const string IDX10202 = "IDX10202: Saml2Token.Assertion is null, can not create an identity.";
-        public const string IDX10203 = "IDX10203: Saml2.Assertion.Issuer is null or whitespace.";
+        public const string IDX10202 = "IDX10202: SamlToken.Assertion is null, can not create an identity.";
+        public const string IDX10203 = "IDX10203: SamlToken.Assertion.Issuer is null or whitespace.";
         public const string IDX10204 = "IDX10204: Unable to validate issuer. validationParameters.ValidIssuer is null or whitespace AND validationParameters.ValidIssuers is null.";
         public const string IDX10205 = "IDX10205: Unable to validate issuer, validationParameters.ValidIssuer: '{0}' or validationParameters.ValidIssuers: '{1}' did not match Issuer: '{2}'. Comparison is: Equals ";
         public const string IDX10206 = "IDX10206: Unable to validate issuer, 'securityToken' type was not a: '{0}', was a: '{1}'";
         public const string IDX10207 = "IDX10207: Unable to validate audience, the Saml2 token did not contain any audiences.";
         public const string IDX10208 = "IDX10208: Unable to validate audience. validationParameters.AllowedAudience is null or whitespace and validationParameters.AllowedAudiences is null.";
+        public const string IDX10209 = "IDX10209: 'tokenString' has length: '{0}' which is larger than the MaximumTokenSizeInBytes: '{1}'.";
     }
 }
