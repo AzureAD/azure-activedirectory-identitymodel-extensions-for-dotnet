814a0e056363cf7a47e5f98a4835fb5b13ba0edc
Brent Schmaltz <brentschmaltz@hotmail.com>
2014-03-05 15:52:40 -0800

    renamed class

--- Microsoft.IdentityModel.Extensions/src/OpenIdConnectException.cs
+++ Microsoft.IdentityModel.Extensions/src/OpenIdConnectException.cs
@@ -6,23 +6,43 @@ using System.Runtime.Serialization;
 namespace Microsoft.IdentityModel.Protocols
 {
     [Serializable]
-    public class OpenIdConnectException : Exception
+    /// <summary>
+    /// This exception is thrown when an OpenIdConnect protocol handler encounters a protocol error.
+    /// </summary>
+    public class OpenIdConnectProtocolException : Exception
     {
-        public OpenIdConnectException()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenIdConnectProtocolException"/> class.
+        /// </summary>
+        public OpenIdConnectProtocolException()
         {
         }
 
-        public OpenIdConnectException(String message)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenIdConnectProtocolException"/> class.
+        /// </summary>
+        /// <param name="message">Addtional information to be included in the exception and displayed to user.</param>
+        public OpenIdConnectProtocolException(String message)
             : base(message)
         {
         }
 
-        public OpenIdConnectException(String message, Exception innerException)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenIdConnectProtocolException"/> class.
+        /// </summary>
+        /// <param name="message">Addtional information to be included in the exception and displayed to user.</param>
+        /// <param name="innerException">A <see cref="Exception"/> that represents the root cause of the exception.</param>
+        public OpenIdConnectProtocolException(String message, Exception innerException)
             : base(message, innerException)
         {
         }
 
-        protected OpenIdConnectException(SerializationInfo info, StreamingContext context)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenIdConnectProtocolException"/> class.
+        /// </summary>
+        /// <param name="info">the <see cref="SerializationInfo"/> that holds the serialized object data.</param>
+        /// <param name="context">The contextual information about the source or destination.</param>
+        protected OpenIdConnectProtocolException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
         }
