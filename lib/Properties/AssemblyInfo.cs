b802a22a81dcce5b9831cdeb11f86c4159ab69ef
Brent Schmaltz <brentschmaltz@hotmail.com>
2014-03-05 07:44:58 -0800

    automatic generation of verion for assembly and nuget

--- lib/Properties/AssemblyInfo.cs
+++ lib/Properties/AssemblyInfo.cs
@@ -1,38 +1,20 @@
-﻿// ----------------------------------------------------------------------------------
-//
-// Copyright Microsoft Corporation
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// ----------------------------------------------------------------------------------
+﻿﻿﻿﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Resources;
 using System;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("System.IdentityModel.Tokens.Jwt")]
 [assembly: AssemblyDescription(".Net 4.5 support for JSON Web Security Tokens")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Microsoft.IdentityModel.Protocol.Extensions")]
+[assembly: AssemblyCopyright("\x00a9 Microsoft Corporation All rights reserved.")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: ComVisible(false)]
 [assembly: Guid("ED55D03C-8832-4483-BD91-1AFAF42C0731")]
-[assembly: AssemblyCompanyAttribute("Microsoft")]
-[assembly: AssemblyProductAttribute("JSON Web Security Token")]
-[assembly: AssemblyCopyrightAttribute("Microsoft")]
-[assembly: AssemblyVersionAttribute("4.0.0.0")]
-[assembly: AssemblyFileVersionAttribute("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("4.0.0.0-alpha")]
-[assembly: ComVisibleAttribute(false)]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
 [assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: AssemblyFileVersion("4.0.10304.1702")]
