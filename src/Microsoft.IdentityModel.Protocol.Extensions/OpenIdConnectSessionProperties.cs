385ba27bdbbdaae8c5d51883f524c9be44fae832
Brent Schmaltz <brentschmaltz@hotmail.com>
2014-06-17 10:29:05 -0700

    Adjusted namespace.

--- src/Microsoft.IdentityModel.Protocol.Extensions/OpenIdConnectSessionProperties.cs
+++ src/Microsoft.IdentityModel.Protocol.Extensions/OpenIdConnectSessionProperties.cs
@@ -16,7 +16,7 @@
 // limitations under the License.
 //-----------------------------------------------------------------------
 
-namespace System.IdentityModel.Tokens
+namespace Microsoft.IdentityModel.Protocols
 {
     /// <summary>
     /// Defines a set of properties names 
