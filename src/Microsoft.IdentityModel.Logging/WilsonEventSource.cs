8a998152fa80698924ae5d4f3bf369cb41e194d5
tushar gupta <tushargupta51@gmail.com>
2015-03-20 15:32:42 -0700

    Cleanup.

--- src/Microsoft.IdentityModel.Logging/WilsonEventSource.cs
+++ src/Microsoft.IdentityModel.Logging/WilsonEventSource.cs
@@ -11,51 +11,51 @@ namespace Microsoft.IdentityModel.Logging
         private static EventLevel _logLevel;
 
         static WilsonEventSource()
-		{
-			Logger = new WilsonEventSource();
+        {
+            Logger = new WilsonEventSource();
             _logLevel = EventLevel.Informational;
-		}
+        }
 
         /// <summary>
         /// Static logger that is exposed externally. An external application or framework can hook up a listener to this event source to log data in a custom way.
         /// </summary>
         public static WilsonEventSource Logger { get; }
 
-		[Event(1, Level = EventLevel.Verbose)]
-		public void WriteVerbose(string message)
-		{
+        [Event(1, Level = EventLevel.Verbose)]
+        public void WriteVerbose(string message)
+        {
             if (_logLevel >= EventLevel.Verbose)
             {
                 WriteEvent(1, message);
             }
         }
 
-		[Event(2, Level = EventLevel.Informational)]
-		public void WriteInformation(string message)
-		{
+        [Event(2, Level = EventLevel.Informational)]
+        public void WriteInformation(string message)
+        {
             if (_logLevel >= EventLevel.Informational)
             {
                 WriteEvent(2, message);
             }
-		}
+        }
 
-		[Event(3, Level = EventLevel.Warning)]
-		public void WriteWarning(string message)
-		{
+        [Event(3, Level = EventLevel.Warning)]
+        public void WriteWarning(string message)
+        {
             if (_logLevel >= EventLevel.Warning)
             {
                 WriteEvent(3, message);
             }
-		}
+        }
 
-		[Event(4, Level = EventLevel.Error)]
-		public void WriteError(string message)
-		{
+        [Event(4, Level = EventLevel.Error)]
+        public void WriteError(string message)
+        {
             if (_logLevel >= EventLevel.Error)
             {
                 WriteEvent(4, message);
             }
-		}
+        }
 
         /// <summary>
         /// Minimum log level to log events. Default is Informational.
@@ -71,5 +71,5 @@ namespace Microsoft.IdentityModel.Logging
                 _logLevel = value;
             }
         }
-	}
+    }
 }
