d4ca06ebcc240cd5e9ea154cd99c85f7055f482f
tushar gupta <tushargupta51@gmail.com>
2015-06-16 18:29:01 -0700

    Adding ValidateSignature boolean flag on TokenValidationParameters.

--- src/Microsoft.IdentityModel.Tokens.Jwt/JwtSecurityTokenHandler.cs
+++ src/Microsoft.IdentityModel.Tokens.Jwt/JwtSecurityTokenHandler.cs
@@ -501,7 +501,15 @@ namespace Microsoft.IdentityModel.Tokens.Jwt
                 LogHelper.Throw(string.Format(CultureInfo.InvariantCulture, ErrorMessages.IDX10209, securityToken.Length, MaximumTokenSizeInBytes), typeof(ArgumentException), EventLevel.Error);
             }
 
-            JwtSecurityToken jwt = this.ValidateSignature(securityToken, validationParameters);
+            JwtSecurityToken jwt = null;
+            if (validationParameters.ValidateSignature)
+            {
+                jwt = this.ValidateSignature(securityToken, validationParameters);
+            }
+            else
+            {
+                jwt = this.ReadToken(securityToken) as JwtSecurityToken;
+            }
 
             if (jwt.SigningKey != null)
             {
