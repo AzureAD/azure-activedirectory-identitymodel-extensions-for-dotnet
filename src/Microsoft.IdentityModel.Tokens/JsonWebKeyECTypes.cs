9e7893844be2ebe125d100ef737c0ac62f3509cb
Polita Paulus <phuff@microsoft.com>
2016-09-26 15:44:59 -0700

    Update JsonWebKeyECTypes.P512 to P521 without breaking changes

--- src/Microsoft.IdentityModel.Tokens/JsonWebKeyECTypes.cs
+++ src/Microsoft.IdentityModel.Tokens/JsonWebKeyECTypes.cs
@@ -37,6 +37,7 @@ namespace Microsoft.IdentityModel.Tokens
         public const string P256 = "P-256";
         public const string P384 = "P-384";
         public const string P512 = "P-512";
+        public const string P521 = "P-521"; // treat 512 as 521. 512 doesn't exist, but we released with "512" instead of "521", so don't break now.
 #pragma warning restore 1591
     }
 }
