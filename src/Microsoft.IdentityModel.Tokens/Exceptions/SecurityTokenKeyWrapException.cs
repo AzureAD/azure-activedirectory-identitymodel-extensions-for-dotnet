9ba257778c3528bdc89848db25673375518f9fe8
Yan Chen <cyahe@microsoft.com>
2016-11-10 11:54:03 -0800

    Rename KeyWrapException to SecurityTokenKeyWrapException

--- src/Microsoft.IdentityModel.Tokens/Exceptions/SecurityTokenKeyWrapException.cs
+++ src/Microsoft.IdentityModel.Tokens/Exceptions/SecurityTokenKeyWrapException.cs
@@ -36,32 +36,32 @@ namespace Microsoft.IdentityModel.Tokens
     /// <summary>
     /// Represents a key wrap exception when encryption failed.
     /// </summary>
-    public class KeyWrapException : SecurityTokenException
+    public class SecurityTokenKeyWrapException : SecurityTokenException
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="KeyWrapException"/> class.
+        /// Initializes a new instance of the <see cref="SecurityTokenKeyWrapException"/> class.
         /// </summary>
-        public KeyWrapException()
+        public SecurityTokenKeyWrapException()
             : base()
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="KeyWrapException"/> class with a specified error message.
+        /// Initializes a new instance of the <see cref="SecurityTokenKeyWrapException"/> class with a specified error message.
         /// </summary>
         /// <param name="message">The error message that explains the reason for the exception.</param>
-        public KeyWrapException(string message)
+        public SecurityTokenKeyWrapException(string message)
             : base(message)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="KeyWrapException"/> class with a specified error message
+        /// Initializes a new instance of the <see cref="SecurityTokenKeyWrapException"/> class with a specified error message
         /// and a reference to the inner exception that is the cause of this exception.
         /// </summary>
         /// <param name="message">The error message that explains the reason for the exception.</param>
         /// <param name="innerException">The <see cref="Exception"/> that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
-        public KeyWrapException(string message, Exception innerException)
+        public SecurityTokenKeyWrapException(string message, Exception innerException)
             : base(message, innerException)
         {
         }
