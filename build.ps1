c3a69311925f2e4640bf71400863a44988ebdecd
RussKie <RussKie@users.noreply.github.com>
2017-02-16 10:13:48 +1100

    Fix $clean handling
    `$clean` like any other input parameter is set to "YES". Presumably the opposite value is expected to be "NO" however the current implementation does not test for "YES" or "NO".

--- build.ps1
+++ build.ps1
@@ -63,7 +63,7 @@ function SetCoreFxVersion([string]$fileName, [string]$oldVersion, [string]$newVe
     Write-Host "============================"
 }
 
-if ($clean)
+if ($clean -eq "YES")
 {
     Write-Host ""
     Write-Host "============================"
