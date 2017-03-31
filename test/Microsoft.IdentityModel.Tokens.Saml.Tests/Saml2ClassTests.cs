c15b1da7875990a842af44244cb58b2137449283
BrentSchmaltz <brentschmaltz@hotmail.com>
2017-03-30 18:42:02 -0700

    Added test project for Xml

---
+++ test/Microsoft.IdentityModel.Tokens.Saml.Tests/Saml2ClassTests.cs
@@ -0,0 +1,50 @@
+//------------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.IdentityModel.Tokens.Saml2;
+using Microsoft.IdentityModel.Tokens.Tests;
+using Xunit;
+
+/// <summary>
+/// Tests all the different Saml2 Types such as Saml2Advice, etc.
+/// </summary>
+namespace Microsoft.IdentityModel.Tokens.Saml.Tests
+{
+    public class Saml2ClassTests
+    {
+
+        [Fact]
+        public void Saml2AdviceTests()
+        {
+
+        }
+    }
+}
