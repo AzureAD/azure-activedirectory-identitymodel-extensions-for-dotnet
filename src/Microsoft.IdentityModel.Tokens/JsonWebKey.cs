69de9281f9e032f738463e2d44314f5f62ff9d1c
Maria Furman <mafurman@microsoft.com>
2017-08-28 15:35:31 -0700

    Deleted the XmlEncryptionException class

--- src/Microsoft.IdentityModel.Tokens/JsonWebKey.cs
+++ src/Microsoft.IdentityModel.Tokens/JsonWebKey.cs
@@ -27,7 +27,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using Microsoft.IdentityModel.Logging;
