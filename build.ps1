d9a73bec4df85a197fc1b10bf7b0f4f7cd4f95bb
BrentSchmaltz <brentschmaltz@hotmail.com>
2018-01-25 13:41:30 -0800

    specify build type to dotnet.exe

--- build.ps1
+++ build.ps1
@@ -144,9 +144,8 @@ if ($build -eq "YES")
 
     Write-Host ">>> Start-Process -wait -NoNewWindow $dotnetexe 'restore' $root\$slnFile"
     Start-Process -wait -NoNewWindow $dotnetexe "restore $root\$slnFile"
-    Write-Host ">>> Start-Process -wait -NoNewWindow $dotnetexe 'build' $root\$slnFile"
-    Start-Process -wait -NoNewWindow $dotnetexe "build $root\$slnFile"
-
+    Write-Host ">>> Start-Process -wait -NoNewWindow $dotnetexe 'build' -c $buildType $root\$slnFile"
+    Start-Process -wait -NoNewWindow $dotnetexe "build -c $buildType $root\$slnFile"
     WriteSectionFooter("End Build");
 }
 
