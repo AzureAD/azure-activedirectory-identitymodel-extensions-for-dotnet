73de2fa1bcd4b1b41f147a368e55c6a2b87f64f9
Maria Furman <mafurman@microsoft.com>
2018-04-09 15:33:12 -0700

    Fixed a bug in the AsymmetricSignatureProvider_Constructor() tests

--- test/Microsoft.IdentityModel.Tests/KeyingMaterial.cs
+++ test/Microsoft.IdentityModel.Tests/KeyingMaterial.cs
@@ -838,7 +838,7 @@ namespace Microsoft.IdentityModel.Tests
                     ""crv"": ""P-521"",
                     ""x"": ""AHKZLLOsCOzz5cY97ewNUajB957y-C-U88c3v13nmGZx6sYl_oJXu9A5RkTKqjqvjyekWF-7ytDyRXYgCF5cj0Kt"",
                     ""y"": ""AdymlHvOiLxXkEhayXQnNCvDX4h9htZaCJN34kfmC6pV5OhQHiraVySsUdaQkAgDPrwQrJmbnX9cwlGfP-HqHZR1"",
-                    ""d"": ""{{0}}""
+                    ""d"": ""{0}""
                     }}", curvePointParameter);
                 
                 return new JsonWebKey(jsonString);
