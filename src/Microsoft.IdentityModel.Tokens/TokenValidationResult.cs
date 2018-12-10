e5df8dd4e10850f0eed4613c1e7612c6636af9dc
Maria Furman <mafurman@microsoft.com>
2018-12-10 15:56:55 -0800

    Moved TokenContext and TokenValidationResult into M.IM.Tokens (#1073)

---
+++ src/Microsoft.IdentityModel.Tokens/TokenValidationResult.cs
@@ -0,0 +1,73 @@
+﻿//------------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+using System;
+using System.Security.Claims;
+
+namespace Microsoft.IdentityModel.Tokens
+{
+    /// <summary>
+    /// Contains artifacts obtained when a SecurityToken is validated.
+    /// </summary>
+    public class TokenValidationResult
+    {
+        /// <summary>
+        /// The <see cref="ClaimsIdentity"/> created from the validated security token.
+        /// </summary>
+        public ClaimsIdentity ClaimsIdentity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="Exception"/> that occurred during validation.
+        /// </summary>
+        public Exception Exception { get; set; }
+
+        /// <summary>
+        /// Gets or sets the issuer that was found in the token.
+        /// </summary>
+        public string Issuer { get; set; }
+
+        /// <summary>
+        /// True if the token was successfully validated, false otherwise.
+        /// </summary>
+        public bool IsValid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="SecurityToken"/> that was validated.
+        /// </summary>
+        public SecurityToken SecurityToken { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="TokenContext"/> that contains call information.
+        /// </summary>
+        public TokenContext TokenContext { get; set; }
+
+        /// <summary>
+        /// Gets or sets the token type of the <see cref="SecurityToken"/> that was validated.
+        /// </summary>
+        public string TokenType { get; set; }
+    }
+}
