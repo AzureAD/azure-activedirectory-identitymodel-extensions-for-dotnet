c4816852269cf346e5e7dff1a1034dffe0e8b3bc
BrentSchmaltz <brentschmaltz@hotmail.com>
2019-01-14 17:57:57 -0800

    simplified updating build scripts
    fixed warning licenceurl is obsolete
    
    # Conflicts:
    #	src/Microsoft.IdentityModel.JsonWebTokens/Properties/AssemblyInfo.cs
    #	src/Microsoft.IdentityModel.KeyVaultExtensions/Properties/AssemblyInfo.cs
    #	src/Microsoft.IdentityModel.Logging/Properties/AssemblyInfo.cs
    #	src/Microsoft.IdentityModel.ManagedKeyVaultSecurityKey/Properties/AssemblyInfo.cs
    #	src/Microsoft.IdentityModel.Protocols.OpenIdConnect/Properties/AssemblyInfo.cs
    #	src/Microsoft.IdentityModel.Protocols.WsFederation/Properties/AssemblyInfo.cs
    #	src/Microsoft.IdentityModel.Protocols/Properties/AssemblyInfo.cs
    #	src/Microsoft.IdentityModel.Tokens.Saml/Properties/AssemblyInfo.cs
    #	src/Microsoft.IdentityModel.Tokens/Properties/AssemblyInfo.cs
    #	src/Microsoft.IdentityModel.Xml/Properties/AssemblyInfo.cs
    #	src/System.IdentityModel.Tokens.Jwt/Properties/AssemblyInfo.cs
    #	updateAssemblyInfo.ps1

--- src/Microsoft.IdentityModel.Logging/Properties/AssemblyInfo.cs
+++ src/Microsoft.IdentityModel.Logging/Properties/AssemblyInfo.cs
@@ -29,9 +29,9 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyInformationalVersion("5.3.0")]
-[assembly: AssemblyFileVersion("5.3.0")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
-[assembly: AssemblyVersion("5.3.0")]
+[assembly: AssemblyVersion("0.0.1")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
