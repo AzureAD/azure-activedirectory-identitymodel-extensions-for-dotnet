92ed8bd2aa5c02de833582d4f624cece67dec6cb
Alexej Kowalew <616b2f@gmail.com>
2019-03-03 19:29:22 +0100

    fix WsUtility ns and WsAddressing comments

--- src/Microsoft.IdentityModel.Protocols.WsFederation/WsUtilityConstants.cs
+++ src/Microsoft.IdentityModel.Protocols.WsFederation/WsUtilityConstants.cs
@@ -28,17 +28,17 @@
 namespace Microsoft.IdentityModel.Xml
 {
     /// <summary>
-    /// Constants for WsAddressing.
+    /// Constants for WsUtility.
     /// </summary>
     public static class WsUtility
     {
         #pragma warning disable 1591
 
-        public const string Namespace = "http://www.w3.org/2005/08/addressing";
+        public const string Namespace = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
         public const string PreferredPrefix = "wsu";
 
         /// <summary>
-        /// Elements that are in the WsAddressing ns
+        /// Elements that are in the WsUtility ns
         /// </summary>
         public static class Elements
         {
