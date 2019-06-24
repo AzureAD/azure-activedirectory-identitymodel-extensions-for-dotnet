80eda1679bd646c0c6baadc92c838e661c14b40f
Brent Schmaltz <brentsch@ntdev.microsoft.com>
2019-06-24 10:27:58 -0700

    simple environment for perf and stress

---
+++ test/Runtime/RuntimeTestCommon/LogMessages.cs
@@ -0,0 +1,17 @@
+﻿//-------------------------------------------------------------------------------------------------
+// <copyright file="LogMessages.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//-------------------------------------------------------------------------------------------------
+
+namespace RuntimeTestCommon
+{
+    /// <summary>
+    /// Log messages and codes.
+    /// </summary>
+    internal static class LogMessages
+    {
+        internal const string PERF10000 = "PERF10000: Certificate with thumbprint: '{0}' could not be found in certificate store with name '{1}' and location '{2}'.";
+        internal const string PERF10001 = "PERF10001: {0} cannot be less than {1}.";
+    }
+}
