584b53bac9b57dfb3e999c9cdb7046e5bf9b3a7f
George Krechar <dokrecar@microsoft.com>
2019-09-23 10:10:12 -0700

    Changed position of HttpRequestData parameter to be consistent with other methods

--- test/Microsoft.IdentityModel.Protocols.PoP.Tests/PopTests.cs
+++ test/Microsoft.IdentityModel.Protocols.PoP.Tests/PopTests.cs
@@ -116,7 +116,7 @@ namespace Microsoft.IdentityModel.Protocols.PoP.Tests
 
             try
             {
-                var popToken = await popHandler.CreatePopTokenAsync(accessTokenWithCnfClaim, signingCredentials, httpRequestData, popCreationPolicy, CancellationToken.None).ConfigureAwait(false);
+                var popToken = await popHandler.CreatePopTokenAsync(accessTokenWithCnfClaim, httpRequestData, signingCredentials,popCreationPolicy, CancellationToken.None).ConfigureAwait(false);
                 var result = await popHandler.ValidatePopTokenAsync(popToken, httpRequestData, tokenValidationParameters, popValidationPolicy, CancellationToken.None).ConfigureAwait(false);
                 //4.1.
                 var popHeader = PopUtilities.CreateHttpRequestPopHeader(result.HttpRequestPopToken);
