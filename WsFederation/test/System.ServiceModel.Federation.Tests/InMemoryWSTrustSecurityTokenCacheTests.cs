17980455170f4e5929fb562da5dacc9be74cea0a
Mike Rousos <MikeRou@Microsoft.com>
2020-03-13 18:04:32 -0400

    Add unit test project for System.ServiceModel.Federation

---
+++ WsFederation/test/System.ServiceModel.Federation.Tests/InMemoryWSTrustSecurityTokenCacheTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.ServiceModel.Caching;
+using System.ServiceModel.Security.Tokens;
+using System.Text;
+using System.Xml;
+using Xunit;
+
+namespace System.ServiceModel.Federation.Tests
+{
+    public class InMemoryWSTrustSecurityTokenCacheTests
+    {
+        [Fact]
+        public void SetAndGetTokens()
+        {
+            var cache = new InMemorySecurityTokenResponseCache<string, string>(EqualityComparer<string>.Default);
+
+            string token1 = "1T";
+            string token2 = "2T";
+            string token3 = "3T";
+            string updatedToken = "UT";
+
+            cache.CacheSecurityTokenResponse("Token1", token1);
+            cache.CacheSecurityTokenResponse("Token2", token2);
+            cache.CacheSecurityTokenResponse("Token3", token3);
+
+            Assert.Equal(token1, cache.GetSecurityTokenResponse("Token1"));
+            Assert.Equal(token2, cache.GetSecurityTokenResponse("Token2"));
+            Assert.Equal(token3, cache.GetSecurityTokenResponse("Token3"));
+            Assert.Null(cache.GetSecurityTokenResponse("Token4"));
+
+            Assert.True(cache.RemoveSecurityTokenResponse(token2));
+            Assert.False(cache.RemoveSecurityTokenResponse(updatedToken));
+            Assert.True(cache.RemoveSecurityTokenResponseByKey("Token3"));
+            Assert.False(cache.RemoveSecurityTokenResponseByKey("token4"));
+
+            Assert.Equal(token1, cache.GetSecurityTokenResponse("Token1"));
+            Assert.Null(cache.GetSecurityTokenResponse("Token2"));
+            Assert.Null(cache.GetSecurityTokenResponse("Token3"));
+
+            cache.CacheSecurityTokenResponse("Token1", updatedToken);
+            Assert.Equal(updatedToken, cache.GetSecurityTokenResponse("Token1"));
+        }
+    }
+}
---
+++ WsFederation/test/System.ServiceModel.Federation.Tests/WSTrustChannelSecurityTokenProviderTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.Text;
+using Microsoft.IdentityModel.Protocols.WsTrust;
+using Microsoft.IdentityModel.Tokens.Saml2;
+using Xunit;
+
+namespace System.ServiceModel.Federation.Tests
+{
+    public class WSTrustChannelSecurityTokenProviderTests
+    {
+
+        [Fact]
+        public void CachingSettingsAreInheritedFromClientCredentials()
+        {
+            // Initialize provider
+            var credentials = new WsTrustChannelClientCredentials()
+            {
+                CacheIssuedTokens = false,
+                IssuedTokenRenewalThresholdPercentage = 80,
+                MaxIssuedTokenCachingTime = TimeSpan.FromDays(1)
+            };
+
+            var tokenRequirements = WSTrustTestHelpers.CreateSecurityRequirement(new BasicHttpBinding());
+
+            var tokenProvider = credentials.CreateSecurityTokenManager().CreateSecurityTokenProvider(tokenRequirements) as WSTrustChannelSecurityTokenProvider;
+
+            // Confirm initial settings are propagated
+            Assert.False(tokenProvider.CacheIssuedTokens);
+            Assert.Equal(80, tokenProvider.IssuedTokenRenewalThresholdPercentage);
+            Assert.Equal(TimeSpan.FromDays(1), tokenProvider.MaxIssuedTokenCachingTime);
+
+            // Change client credential settings
+            credentials.CacheIssuedTokens = true;
+            credentials.IssuedTokenRenewalThresholdPercentage = 1;
+            credentials.MaxIssuedTokenCachingTime = TimeSpan.MaxValue;
+            tokenProvider = credentials.CreateSecurityTokenManager().CreateSecurityTokenProvider(tokenRequirements) as WSTrustChannelSecurityTokenProvider;
+
+            // Confirm updated settings are propagated
+            Assert.True(tokenProvider.CacheIssuedTokens);
+            Assert.Equal(1, tokenProvider.IssuedTokenRenewalThresholdPercentage);
+            Assert.Equal(TimeSpan.MaxValue, tokenProvider.MaxIssuedTokenCachingTime);
+        }
+    }
+}
