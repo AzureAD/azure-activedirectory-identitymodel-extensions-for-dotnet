34ce9ace6192bf692565eefac108ecc2b0f4d6bc
Mike Rousos <MikeRou@Microsoft.com>
2020-04-01 14:15:39 -0400

    Add tests for holder-of-key scenarios

---
+++ test/System.ServiceModel.Federation.Tests/Mocks/MockResponseSettings.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Protocols.WsTrust;
+
+namespace System.ServiceModel.Federation.Tests.Mocks
+{
+    /// <summary>
+    /// Class used to store settings a mock channel should use when generating mock responses.
+    /// </summary>
+    public class MockResponseSettings
+    {
+        public Entropy Entropy { get; set; }
+
+        public int? KeySizeInBits { get; set; }
+
+        public string KeyType { get; set; }
+
+        public Lifetime Lifetime { get; set; }
+
+        public RequestedProofToken ProofToken { get; set; }
+    }
+}
