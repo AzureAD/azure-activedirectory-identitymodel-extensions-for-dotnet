29141b34f27896e9cb6252a27f88d3af48a91596
brentschmaltz <brentschmaltz@hotmail.com>
2021-03-18 13:55:12 -0700

    improve speed of urlencoder by reducing string copies

--- src/Microsoft.IdentityModel.Tokens/Base64UrlEncoder.cs
+++ src/Microsoft.IdentityModel.Tokens/Base64UrlEncoder.cs
@@ -37,11 +37,13 @@ namespace Microsoft.IdentityModel.Tokens
     public static class Base64UrlEncoder
     {
         private static char base64PadCharacter = '=';
+#if NET45
         private static string doubleBase64PadCharacter = "==";
+#endif
         private static char base64Character62 = '+';
         private static char base64Character63 = '/';
         private static char base64UrlCharacter62 = '-';
-        private static char _base64UrlCharacter63 = '_';
+        private static char base64UrlCharacter63 = '_';
 
         /// <summary>
         /// The following functions perform base64url encoding which differs from regular base64 encoding as follows
@@ -66,7 +68,7 @@ namespace Microsoft.IdentityModel.Tokens
         /// <param name="inArray">An array of 8-bit unsigned integers.</param>
         /// <param name="length">An offset in inArray.</param>
         /// <param name="offset">The number of elements of inArray to convert.</param>
-        /// <returns>The string representation in base 64 url encodingof length elements of inArray, starting at position offset.</returns>
+        /// <returns>The string representation in base 64 url encoding of length elements of inArray, starting at position offset.</returns>
         /// <exception cref="ArgumentNullException">'inArray' is null.</exception>
         /// <exception cref="ArgumentOutOfRangeException">offset or length is negative OR offset plus length is greater than the length of inArray.</exception>
         public static string Encode(byte[] inArray, int offset, int length)
@@ -74,11 +76,7 @@ namespace Microsoft.IdentityModel.Tokens
             if (inArray == null)
                 throw LogHelper.LogArgumentNullException("inArray");
 
-            string s = Convert.ToBase64String(inArray, offset, length);
-            s = s.Split(base64PadCharacter)[0]; // Remove any trailing padding
-            s = s.Replace(base64Character62, base64UrlCharacter62);  // 62nd char of encoding
-            s = s.Replace(base64Character63, _base64UrlCharacter63);  // 63rd char of encoding
-            return s;
+            return EncodeString(Convert.ToBase64String(inArray, offset, length));
         }
 
         /// <summary>
@@ -86,7 +84,7 @@ namespace Microsoft.IdentityModel.Tokens
         /// the subset as an offset in the input array, and the number of elements in the array to convert.
         /// </summary>
         /// <param name="inArray">An array of 8-bit unsigned integers.</param>
-        /// <returns>The string representation in base 64 url encodingof length elements of inArray, starting at position offset.</returns>
+        /// <returns>The string representation in base 64 url encoding of length elements of inArray, starting at position offset.</returns>
         /// <exception cref="ArgumentNullException">'inArray' is null.</exception>
         /// <exception cref="ArgumentOutOfRangeException">offset or length is negative OR offset plus length is greater than the length of inArray.</exception>
         public static string Encode(byte[] inArray)
@@ -94,13 +92,81 @@ namespace Microsoft.IdentityModel.Tokens
             if (inArray == null)
                 throw LogHelper.LogArgumentNullException("inArray");
 
-            string s = Convert.ToBase64String(inArray, 0, inArray.Length);
-            s = s.Split(base64PadCharacter)[0]; // Remove any trailing padding
-            s = s.Replace(base64Character62, base64UrlCharacter62);  // 62nd char of encoding
-            s = s.Replace(base64Character63, _base64UrlCharacter63);  // 63rd char of encoding
-
-            return s;
+            return EncodeString(Convert.ToBase64String(inArray, 0, inArray.Length));
         }
+
+        internal static string EncodeString(string str)
+        {
+#if NET45
+            str = str.Split(base64PadCharacter)[0]; // Remove any trailing padding
+            str = str.Replace(base64Character62, base64UrlCharacter62);  // 62nd char of encoding
+            str = str.Replace(base64Character63, base64UrlCharacter63);  // 63rd char of encoding
+
+            return str;
+#else
+            return UnsafeEncode(str);
+#endif
+        }
+
+#if !NET45
+        private unsafe static string UnsafeEncode(string str)
+        {
+            bool needReplace = false;
+            int reductionSize = 0;
+            if (str[str.Length - 1] == base64PadCharacter)
+                reductionSize = 1;
+
+            if (str[str.Length - 2] == base64PadCharacter)
+                reductionSize = 2;
+
+            int encodedLength = str.Length - reductionSize;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == base64Character62 || str[i] == base64Character63)
+                {
+                    needReplace = true;
+                    break;
+                }
+            }
+
+            if (needReplace)
+            {
+                string encodedString = new string(char.MinValue, encodedLength);
+                fixed (char* dest = encodedString)
+                {
+                    for (int i = 0; i < encodedLength; i++)
+                    {
+                        if (str[i] == base64Character62)
+                            dest[i] = base64UrlCharacter62;
+                        else if (str[i] == base64Character63)
+                            dest[i] = base64UrlCharacter63;
+                        else
+                            dest[i] = str[i];
+                    }
+                }
+
+                return encodedString;
+            }
+            else
+            {
+                if (encodedLength == str.Length)
+                {
+                    return str;
+                }
+                else
+                {
+                    string encodedString = new string(char.MinValue, encodedLength);
+                    fixed (char* src = str)
+                    fixed (char* dest = encodedString)
+                    {
+                        Buffer.MemoryCopy(src, dest, encodedLength * 2, encodedLength * 2);
+                    }
+
+                    return encodedString;
+                }
+            }
+        }
+#endif
 
         /// <summary>
         ///  Converts the specified string, which encodes binary data as base-64-url digits, to an equivalent 8-bit unsigned integer array.</summary>
@@ -109,15 +175,14 @@ namespace Microsoft.IdentityModel.Tokens
         public static byte[] DecodeBytes(string str)
         {
             if (str == null)
-            {
                 throw LogHelper.LogExceptionMessage(new ArgumentNullException(nameof(str)));
-            }
 
+#if NET45
             // 62nd char of encoding
             str = str.Replace(base64UrlCharacter62, base64Character62);
             
             // 63rd char of encoding
-            str = str.Replace(_base64UrlCharacter63, base64Character63);
+            str = str.Replace(base64UrlCharacter63, base64Character63);
 
             // check for padding
             switch (str.Length % 4)
@@ -138,7 +203,75 @@ namespace Microsoft.IdentityModel.Tokens
             }
 
             return Convert.FromBase64String(str);
+#else
+            return UnsafeDecode(str);
+#endif
         }
+
+#if !NET45
+        private unsafe static byte[] UnsafeDecode(string str)
+        {
+            int mod = str.Length % 4;
+            if (mod == 1)
+                throw LogHelper.LogExceptionMessage(new FormatException(LogHelper.FormatInvariant(LogMessages.IDX10400, str)));
+
+            bool needReplace = false;
+            int decodedLength = str.Length + (4 - mod) % 4;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == base64UrlCharacter62 || str[i] == base64UrlCharacter63)
+                {
+                    needReplace = true;
+                    break;
+                }
+            }
+
+            if (needReplace)
+            {
+                string decodedString = new string(char.MinValue, decodedLength);
+                fixed (char* dest = decodedString)
+                {
+                    int i = 0;
+                    for (; i < str.Length; i++)
+                    {
+                        if (str[i] == base64UrlCharacter62)
+                            dest[i] = base64Character62;
+                        else if (str[i] == base64UrlCharacter63)
+                            dest[i] = base64Character63;
+                        else
+                            dest[i] = str[i];
+                    }
+
+                    for (; i < decodedLength; i++)
+                        dest[i] = base64PadCharacter;
+                }
+
+                return Convert.FromBase64String(decodedString);
+            }
+            else
+            {
+                if (decodedLength == str.Length)
+                {
+                    return Convert.FromBase64String(str);
+                }
+                else
+                {
+                    string decodedString = new string(char.MinValue, decodedLength);
+                    fixed (char* src = str)
+                    fixed (char* dest = decodedString)
+                    {
+                        Buffer.MemoryCopy(src, dest, str.Length * 2, str.Length * 2);
+                        dest[str.Length] = base64PadCharacter;
+                        if (str.Length + 2 == decodedLength)
+                            dest[str.Length + 1] = base64PadCharacter;
+                    }
+
+                    return Convert.FromBase64String(decodedString);
+                }
+            }
+        }
+#endif
 
         /// <summary>
         /// Decodes the string from Base64UrlEncoded to UTF8.
