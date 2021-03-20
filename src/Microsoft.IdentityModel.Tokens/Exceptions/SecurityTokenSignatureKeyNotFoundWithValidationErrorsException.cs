c68bb7e017605a11eb911326fcafa060b1608b9b
Keegan Caruso <Keegan.Caruso@microsoft.com>
2021-03-20 07:34:46 -0700

    SecurityTokenSignatureKeyNotFoundWithValidationErrorsException message
    Update message to indicate it is not used for metadata refresh

--- src/Microsoft.IdentityModel.Tokens/Exceptions/SecurityTokenSignatureKeyNotFoundWithValidationErrorsException.cs
+++ src/Microsoft.IdentityModel.Tokens/Exceptions/SecurityTokenSignatureKeyNotFoundWithValidationErrorsException.cs
@@ -32,7 +32,8 @@ namespace Microsoft.IdentityModel.Tokens
 {
     /// <summary>
     /// This exception is thrown when a security token contained a key identifier but the key was not found by the runtime.
-    /// In addition, when validation errors exist over the security token.
+    /// In addition, when validation errors exist over the security token. This exception is not intended to be used as a signal
+    /// to refresh keys.
     /// </summary>
     [Serializable]
     public class SecurityTokenSignatureKeyNotFoundWithValidationErrorsException
