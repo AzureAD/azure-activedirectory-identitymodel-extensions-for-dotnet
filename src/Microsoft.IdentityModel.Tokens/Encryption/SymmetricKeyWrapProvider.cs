d73d54ca797c8bc70a3105da851b1c63f7654611
Cesar Payan <cesarpayan@microsoft.com>
2021-08-06 09:28:23 -0700

    Testing showed that we can safely change to CBC CipherMode

--- src/Microsoft.IdentityModel.Tokens/Encryption/SymmetricKeyWrapProvider.cs
+++ src/Microsoft.IdentityModel.Tokens/Encryption/SymmetricKeyWrapProvider.cs
@@ -180,7 +180,7 @@ namespace Microsoft.IdentityModel.Tokens
             {
                 // Create the AES provider
                 SymmetricAlgorithm symmetricAlgorithm = Aes.Create();
-                symmetricAlgorithm.Mode = CipherMode.ECB;
+                symmetricAlgorithm.Mode = CipherMode.CBC;
                 symmetricAlgorithm.Padding = PaddingMode.None;
                 symmetricAlgorithm.KeySize = keyBytes.Length * 8;
                 symmetricAlgorithm.Key = keyBytes;
