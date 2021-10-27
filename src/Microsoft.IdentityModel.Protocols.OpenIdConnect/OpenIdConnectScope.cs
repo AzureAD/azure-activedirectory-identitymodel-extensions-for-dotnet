0faea95449190eeb296e994ba9f4cd1762e2e752
Jonathan <vanillajonathan@users.noreply.github.com>
2021-10-27 18:41:06 +0200

    Add const for the standard scope "phone" (#1720)

--- src/Microsoft.IdentityModel.Protocols.OpenIdConnect/OpenIdConnectScope.cs
+++ src/Microsoft.IdentityModel.Protocols.OpenIdConnect/OpenIdConnectScope.cs
@@ -56,6 +56,11 @@ namespace Microsoft.IdentityModel.Protocols.OpenIdConnect
         public const string OpenIdProfile = "openid profile";
 
         /// <summary>
+        /// Indicates phone profile scope see: https://openid.net/specs/openid-connect-core-1_0.html#ScopeClaims.
+        /// </summary>
+        public const string Phone = "phone";
+
+        /// <summary>
         /// Indicates user_impersonation scope see: http://openid.net/specs/openid-connect-core-1_0.html#ScopeClaims.
         /// </summary>
         public const string UserImpersonation = "user_impersonation";
