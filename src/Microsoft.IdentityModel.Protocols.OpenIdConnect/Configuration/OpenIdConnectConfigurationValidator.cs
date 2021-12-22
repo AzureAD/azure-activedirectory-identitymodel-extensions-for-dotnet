816fd05d56154e12a223c33c3b2241300957d2f2
Xiao Zhang <zhangxia@microsoft.com>
2021-12-21 16:34:14 -0800

    init commit

---
+++ src/Microsoft.IdentityModel.Protocols.OpenIdConnect/Configuration/OpenIdConnectConfigurationValidator.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.IdentityModel.Protocols.OpenIdConnect.Configuration
+{
+    /// <summary>
+    /// Validates OpenIdConnectConfiguration.
+    /// </summary>
+    public class OpenIdConnectConfigurationValidator
+    {
+        /// <summary>
+        /// Additional validation policy.
+        /// </summary>
+        public List<IConfigurationValidationPolicy> AdditionalConfigurationValidaionPolicies { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<ConfigurationValidationResult> ConfigurationValidationResults { get; } = new List<ConfigurationValidationResult>();
+
+        /// <summary>
+        /// Validates a OpenIdConnectConfiguration by using current policy set.
+        /// </summary>
+        /// <param name="openIdConnectConfiguration">The OpenIdConnectConfiguration to validate</param>
+        /// <returns></returns>
+        public List<ConfigurationValidationResult> Validate(OpenIdConnectConfiguration openIdConnectConfiguration)
+        {
+            if (openIdConnectConfiguration == null)
+                throw new ArgumentNullException(nameof(openIdConnectConfiguration));
+
+            var allPolicies = new List<IConfigurationValidationPolicy>
+            {
+                new ConfigurationValidationPolicy()
+            };
+            if (AdditionalConfigurationValidaionPolicies != null)
+                allPolicies.AddRange(AdditionalConfigurationValidaionPolicies);
+
+            foreach (IConfigurationValidationPolicy policy in allPolicies)
+            {
+                ConfigurationValidationResult result = policy.ValidateConfiguration(openIdConnectConfiguration);
+                if (!result.Succeeded)
+                {
+                    ConfigurationValidationResults.Add(result);
+                }
+            }
+            return ConfigurationValidationResults;
+            // Or throw exception
+        }
+    }
+}
