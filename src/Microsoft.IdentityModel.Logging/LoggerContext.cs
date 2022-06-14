1503bcdb1ec5047bb95840252d68b793f8cdb0cd
sruke <73967733+sruke@users.noreply.github.com>
2022-06-13 22:07:58 -0700

    Addressed feedback. Added test.

--- src/Microsoft.IdentityModel.Logging/LoggerContext.cs
+++ src/Microsoft.IdentityModel.Logging/LoggerContext.cs
@@ -1,29 +1,5 @@
-﻿//------------------------------------------------------------------------------
-//
-// Copyright (c) Microsoft Corporation.
-// All rights reserved.
-//
-// This code is licensed under the MIT License.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files(the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions :
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
-//------------------------------------------------------------------------------
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 using System;
 using System.Collections.Generic;
@@ -32,12 +8,12 @@ using System.Collections.ObjectModel;
 namespace Microsoft.IdentityModel.Logging
 {
     /// <summary>
-    /// An opaque context used to store work when working with authentication artifacts.
+    /// A context class that can be used to store work per request to aid with debugging.
     /// </summary>
     public class LoggerContext
     {
         /// <summary>
-        /// Instantiates a new <see cref="LoggerContext"/> with a default activityId.
+        /// Instantiates a new <see cref="LoggerContext"/> with the default activityId.
         /// </summary>
         public LoggerContext()
         {
@@ -53,11 +29,6 @@ namespace Microsoft.IdentityModel.Logging
         }
 
         /// <summary>
-        /// Gets or sets a string that helps with setting breakpoints when debugging.
-        /// </summary>
-        public string Id { get; set; } = string.Empty;
-
-        /// <summary>
         /// Gets or set a <see cref="Guid"/> that will be used in the call to EventSource.SetCurrentThreadActivityId before logging.
         /// </summary>
         public Guid ActivityId { get; set; } = Guid.Empty;
@@ -67,6 +38,11 @@ namespace Microsoft.IdentityModel.Logging
         /// Useful when debugging.
         /// </summary>
         public bool CaptureLogs { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets a string that helps with setting breakpoints when debugging.
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
 
         /// <summary>
         /// The collection of logs associated with a request. Use <see cref="CaptureLogs"/> to control capture.
