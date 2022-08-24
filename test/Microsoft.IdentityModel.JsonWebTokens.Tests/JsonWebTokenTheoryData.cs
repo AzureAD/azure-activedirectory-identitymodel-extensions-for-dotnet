e45095a2c765a0e7022a03f05d2b7ddce43d9a4a
cesarpayan <cesarpayan@microsoft.com>
2022-08-24 14:51:53 -0700

    Adding context to unit test

--- test/Microsoft.IdentityModel.JsonWebTokens.Tests/JsonWebTokenTheoryData.cs
+++ test/Microsoft.IdentityModel.JsonWebTokens.Tests/JsonWebTokenTheoryData.cs
@@ -1,11 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Microsoft.IdentityModel.TestUtils;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Microsoft.IdentityModel.JsonWebTokens.Tests
 {
-    public class JsonWebTokenTheoryData
+    public class JsonWebTokenTheoryData : TheoryDataBase
     {
         public TokenValidationParameters ValidationParameters { get; set; }
 
