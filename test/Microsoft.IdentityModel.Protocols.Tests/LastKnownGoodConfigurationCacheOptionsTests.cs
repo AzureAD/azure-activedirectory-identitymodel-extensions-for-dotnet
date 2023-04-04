f098f442765e7c4006ae80786a52da72fed8c1ac
dannybtsai <87037663+dannybtsai@users.noreply.github.com>
2023-04-04 15:55:25 -0700

    added a test

---
+++ test/Microsoft.IdentityModel.Protocols.Tests/LastKnownGoodConfigurationCacheOptionsTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.IdentityModel.Protocols.Configuration;
+using Microsoft.IdentityModel.Tokens.Configuration;
+using Xunit;
+
+namespace Microsoft.IdentityModel.Protocols.Tests
+{
+    public class LastKnownGoodConfigurationCacheOptionsTests
+    {
+        [Fact]
+        public void ImplicitConversionToLKGConfigurationCacheOptions()
+        {
+            LastKnownGoodConfigurationCacheOptions options = new LastKnownGoodConfigurationCacheOptions
+            {
+                LastKnownGoodConfigurationSizeLimit = 999
+            };
+
+            LKGConfigurationCacheOptions implicitlyConvertedOptions = options;
+            Assert.Equal(options.LastKnownGoodConfigurationSizeLimit, implicitlyConvertedOptions.LastKnownGoodConfigurationSizeLimit);
+            Assert.True(Object.ReferenceEquals(options.BaseConfigurationComparer, implicitlyConvertedOptions.BaseConfigurationComparer));
+        }
+    }
+}
