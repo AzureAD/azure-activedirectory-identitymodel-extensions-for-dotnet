5bef9292f663046c5e9d07898cb804f0a4971850
zhangxia_microsoft <zhangxia@microsoft.com>
2023-05-10 16:16:25 -0700

    rename exception

--- src/Microsoft.IdentityModel.Protocols/Configuration/ConfigurationInvalidException.cs
+++ src/Microsoft.IdentityModel.Protocols/Configuration/ConfigurationInvalidException.cs
@@ -10,40 +10,40 @@ namespace Microsoft.IdentityModel.Protocols.Configuration
     /// This exception is thrown when retrieved configuration is not valid.
     /// </summary>
     [Serializable]
-    public class ConfigurationValidationException : Exception
+    public class ConfigurationInvalidException : Exception
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConfigurationValidationException"/> class.
+        /// Initializes a new instance of the <see cref="ConfigurationInvalidException"/> class.
         /// </summary>
-        public ConfigurationValidationException()
+        public ConfigurationInvalidException()
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConfigurationValidationException"/> class.
+        /// Initializes a new instance of the <see cref="ConfigurationInvalidException"/> class.
         /// </summary>
         /// <param name="message">Addtional information to be included in the exception and displayed to user.</param>
-        public ConfigurationValidationException(string message)
+        public ConfigurationInvalidException(string message)
             : base(message)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConfigurationValidationException"/> class.
+        /// Initializes a new instance of the <see cref="ConfigurationInvalidException"/> class.
         /// </summary>
         /// <param name="message">Addtional information to be included in the exception and displayed to user.</param>
         /// <param name="innerException">A <see cref="Exception"/> that represents the root cause of the exception.</param>
-        public ConfigurationValidationException(string message, Exception innerException)
+        public ConfigurationInvalidException(string message, Exception innerException)
             : base(message, innerException)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConfigurationValidationException"/> class.
+        /// Initializes a new instance of the <see cref="ConfigurationInvalidException"/> class.
         /// </summary>
         /// <param name="info">the <see cref="SerializationInfo"/> that holds the serialized object data.</param>
         /// <param name="context">The contextual information about the source or destination.</param>
-        protected ConfigurationValidationException(SerializationInfo info, StreamingContext context)
+        protected ConfigurationInvalidException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
         }
