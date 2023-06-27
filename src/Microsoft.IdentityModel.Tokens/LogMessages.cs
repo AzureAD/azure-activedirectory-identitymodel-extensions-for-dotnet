befc1c876d2ba90dcfef2baf9a88b3dbfcd98002
Keegan Caruso <Keegan.Caruso@microsoft.com>
2023-06-27 13:10:35 -0700

    remove log message IDX10692 (#2125)
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>

--- src/Microsoft.IdentityModel.Tokens/LogMessages.cs
+++ src/Microsoft.IdentityModel.Tokens/LogMessages.cs
@@ -192,7 +192,7 @@ namespace Microsoft.IdentityModel.Tokens
         public const string IDX10689 = "IDX10689: Unable to create an ECDsa object. See inner exception for more details.";
         public const string IDX10690 = "IDX10690: ECDsa creation is not supported by the current platform. For more details, see https://aka.ms/IdentityModel/create-ecdsa";
         //public const string IDX10691 = "IDX10691:"
-        public const string IDX10692 = "IDX10692: The RSASS-PSS signature algorithm is not available on the .NET 4.5 target. The list of supported algorithms is available here: https://aka.ms/IdentityModel/supported-algorithms";
+        //public const string IDX10692 = "IDX10692: The RSASS-PSS signature algorithm is not available on the .NET 4.5 target. The list of supported algorithms is available here: https://aka.ms/IdentityModel/supported-algorithms";
         public const string IDX10693 = "IDX10693: RSACryptoServiceProvider doesn't support the RSASSA-PSS signature algorithm. The list of supported algorithms is available here: https://aka.ms/IdentityModel/supported-algorithms";
         public const string IDX10694 = "IDX10694: JsonWebKeyConverter threw attempting to convert JsonWebKey: '{0}'. Exception: '{1}'.";
         public const string IDX10695 = "IDX10695: Unable to create a JsonWebKey from an ECDsa object. Required ECParameters structure is not supported by .NET Framework < 4.7.";
