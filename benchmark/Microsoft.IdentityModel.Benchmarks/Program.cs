8aca66fde0f00fc92a415dc5f8bd7e0c757c9180
Westin Musser <127992899+westin-m@users.noreply.github.com>
2023-08-23 15:55:00 -0700

    Create microbenchmark for ValidateTokenAsync (#2235)
    * Create microbenchmark for ValidateTokenAsync
    
    * PR fixes, additional tests
    
    * rename methods
    
    * Create claims list for benchmark

---
+++ benchmark/Microsoft.IdentityModel.Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using BenchmarkDotNet.Running;
+
+namespace Microsoft.IdentityModel.Benchmarks
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+}
