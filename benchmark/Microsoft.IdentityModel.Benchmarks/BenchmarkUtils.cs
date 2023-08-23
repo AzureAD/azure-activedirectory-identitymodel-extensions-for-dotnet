59c10b3c5e90d9f4fd1f9e25e02fc217e9ef8b89
Westin Musser <127992899+westin-m@users.noreply.github.com>
2023-08-23 15:55:00 -0700

    Create microbenchmark for ValidateTokenAsync (#2235)
    * Create microbenchmark for ValidateTokenAsync
    
    * PR fixes, additional tests
    
    * rename methods
    
    * Create claims list for benchmark

---
+++ benchmark/Microsoft.IdentityModel.Benchmarks/BenchmarkUtils.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.TestUtils;
+using System.Collections.Generic;
+
+namespace Microsoft.IdentityModel.Benchmarks
+{
+    public class BenchmarkUtils
+    {
+        public static Dictionary<string, object> SimpleClaims
+        {
+            get => new Dictionary<string, object>()
+            {
+                { "role", new List<string>() { "role1", "Developer", "Sales"} },
+                { "email", "Bob@contoso.com" },
+                { "exp", EpochTime.GetIntDate(Default.Expires).ToString() },
+                { "nbf", EpochTime.GetIntDate(Default.NotBefore).ToString() },
+                { "iat", EpochTime.GetIntDate(Default.IssueInstant).ToString() }
+            };
+        }
+    }
+}
---
+++ benchmark/Microsoft.IdentityModel.Benchmarks/ValidateTokenAsyncTests.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using BenchmarkDotNet.Attributes;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.TestUtils;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
+
+namespace Microsoft.IdentityModel.Benchmarks
+{
+    [Config(typeof(AntiVirusFriendlyConfig))]
+    public class ValidateTokenAsyncTests
+    {
+        JsonWebTokenHandler jsonWebTokenHandler;
+        JwtSecurityTokenHandler jwtSecurityTokenHandler;
+        SecurityTokenDescriptor tokenDescriptor;
+        string jsonWebToken;
+        TokenValidationParameters validationParameters;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Claims = BenchmarkUtils.SimpleClaims,
+                SigningCredentials = KeyingMaterial.JsonWebKeyRsa256SigningCredentials,
+            };
+            jsonWebToken = jsonWebTokenHandler.CreateToken(tokenDescriptor);
+            validationParameters = new TokenValidationParameters()
+            {
+                ValidAudience = "http://Default.Audience.com",
+                ValidateLifetime = true,
+                ValidIssuer = "http://Default.Issuer.com",
+                IssuerSigningKey = KeyingMaterial.JsonWebKeyRsa256SigningCredentials.Key,
+            };
+        }
+
+        [GlobalSetup(Targets = new[] { nameof(JsonWebTokenHandler_ValidateTokenAsync) })]
+        public void JsonWebTokenSetup()
+        {
+            jsonWebTokenHandler = new JsonWebTokenHandler();
+            jsonWebTokenHandler.SetDefaultTimesOnTokenCreation = false;
+        }
+
+        [Benchmark]
+        public async Task<TokenValidationResult> JsonWebTokenHandler_ValidateTokenAsync() => await jsonWebTokenHandler.ValidateTokenAsync(jsonWebToken, validationParameters).ConfigureAwait(false);
+
+        [GlobalSetup(Targets = new[] { nameof(JwtSecurityTokenHandler_ValidateTokenAsync) })]
+        public void JwtSecurityTokenSetup()
+        {
+            jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            jwtSecurityTokenHandler.SetDefaultTimesOnTokenCreation = false;
+        }
+
+        [Benchmark]
+        public async Task<TokenValidationResult> JwtSecurityTokenHandler_ValidateTokenAsync() => await jwtSecurityTokenHandler.ValidateTokenAsync(jsonWebToken, validationParameters).ConfigureAwait(false);
+
+    }
+}
