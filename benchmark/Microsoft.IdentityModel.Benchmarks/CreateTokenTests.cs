9846036bbbef09112ae4555f27b6bfcb298ea9e4
Westin Musser <127992899+westin-m@users.noreply.github.com>
2023-08-23 15:55:00 -0700

    Create microbenchmark for ValidateTokenAsync (#2235)
    * Create microbenchmark for ValidateTokenAsync
    
    * PR fixes, additional tests
    
    * rename methods
    
    * Create claims list for benchmark

---
+++ benchmark/Microsoft.IdentityModel.Benchmarks/CreateTokenTests.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Security.Claims;
+using BenchmarkDotNet.Attributes;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.TestUtils;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Microsoft.IdentityModel.Benchmarks
+{
+    public class CreateTokenTests
+    {
+        JsonWebTokenHandler jsonWebTokenHandler;
+        SecurityTokenDescriptor tokenDescriptor;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            jsonWebTokenHandler = new JsonWebTokenHandler();
+            tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(Default.PayloadClaims),
+                SigningCredentials = KeyingMaterial.JsonWebKeyRsa256SigningCredentials,
+            };
+        }
+
+        [Benchmark]
+        public string JsonWebTokenHandler_CreateToken() => jsonWebTokenHandler.CreateToken(tokenDescriptor);
+
+    }
+}
