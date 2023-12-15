f019c4e886ae51141a07cea48acd439059ca648b
Brent Schmaltz <brentsch@hotmail.com>
2023-12-15 12:21:23 -0800

    Remove custom columns and rename derived config make benchmarks consistent

---
+++ benchmark/Microsoft.IdentityModel.Benchmarks/BenchmarkConfig.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+namespace Microsoft.IdentityModel.Benchmarks
+{
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            AddJob(Job.MediumRun
+                .WithToolchain(InProcessEmitToolchain.Instance))
+            .AddColumn(StatisticColumn.P90, StatisticColumn.P95, StatisticColumn.P100);
+        }
+    }
+}
