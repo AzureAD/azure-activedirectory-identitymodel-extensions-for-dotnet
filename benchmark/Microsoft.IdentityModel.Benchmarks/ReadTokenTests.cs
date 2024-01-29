653c347d0b584302692e1631b5ed6e7297ec0a7d
Sruthi Keerthi Rangavajhula <srangavajhul@microsoft.com>
2024-01-29 11:15:15 -0800

    Add benchmark

---
+++ benchmark/Microsoft.IdentityModel.Benchmarks/ReadTokenTests.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using BenchmarkDotNet.Attributes;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace Microsoft.IdentityModel.Benchmarks
+{
+    // dotnet run -c release -f net8.0 --filter Microsoft.IdentityModel.Benchmarks.ReadTokenTests*
+
+    [Config(typeof(BenchmarkConfig))]
+    [HideColumns("Type", "Job", "WarmupCount", "LaunchCount")]
+    [MemoryDiagnoser]
+    public class ReadTokenTests
+    {
+        [Benchmark]
+        public JsonWebToken ReadJWTFromEncodedString()
+        {
+            return new JsonWebToken(EncodedJWTs.Asymmetric_LocalSts);
+        }
+
+        [Benchmark]
+        public JsonWebToken ReadJWTFromEncodedSpan()
+        {
+            return new JsonWebToken(EncodedJWTs.Asymmetric_LocalStsSpan);
+        }
+    }
+}
