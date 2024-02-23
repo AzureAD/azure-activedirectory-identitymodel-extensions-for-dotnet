1fb551d4107af33998a8136bcf283d6410dfbcfa
Franco Fung <38921563+FuPingFranco@users.noreply.github.com>
2024-02-23 10:51:38 -0800

    Replace AssemblyVersion with Version (#2494)
    * Initial commit for changing versioning in Wilson
    
    * Fix pack error
    
    * Removed unenessary steps from dotnet.yml
    
    * Added Product.Proj as target for pack
    
    ---------
    
    Co-authored-by: Franco Fung <francofung@microsoft.com>

--- src/Microsoft.IdentityModel.ManagedKeyVaultSecurityKey/Properties/AssemblyInfo.cs
+++ src/Microsoft.IdentityModel.ManagedKeyVaultSecurityKey/Properties/AssemblyInfo.cs
@@ -5,9 +5,6 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyInformationalVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
-[assembly: AssemblyVersion("0.0.1")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
