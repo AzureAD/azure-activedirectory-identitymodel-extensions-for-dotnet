dfb20fd309d196001ca5ba366cab2dccc36d1d0f
Franco Fung <francofung@microsoft.com>
2024-04-24 10:29:33 -0700

    Adding comment

--- test/System.IdentityModel.Tokens.Jwt.Tests/JwtPayloadTest.cs
+++ test/System.IdentityModel.Tokens.Jwt.Tests/JwtPayloadTest.cs
@@ -59,7 +59,7 @@ namespace System.IdentityModel.Tokens.Jwt.Tests
             Type type = typeof(JwtPayload);
             PropertyInfo[] properties = type.GetProperties();
 #if NET9_0_OR_GREATER
-            if (properties.Length != 26)
+            if (properties.Length != 26) //Additional inherited "Capacity" property from Dictionary is added in .NET 9 
                 Assert.True(false, "Number of properties has changed from 26 to: " + properties.Length + ", adjust tests");
 #else
             if (properties.Length != 25)
