dd7f52d074bd8a1f62043167ddce061b69f3bb45
Franco Fung <38921563+FuPingFranco@users.noreply.github.com>
2024-05-16 10:40:56 -0700

    Update test/System.IdentityModel.Tokens.Jwt.Tests/JwtSecurityTokenHandlerTests.WithContextSwitches.cs
    Co-authored-by: kellyyangsong <69649063+kellyyangsong@users.noreply.github.com>

--- test/System.IdentityModel.Tokens.Jwt.Tests/JwtSecurityTokenHandlerTests.WithContextSwitches.cs
+++ test/System.IdentityModel.Tokens.Jwt.Tests/JwtSecurityTokenHandlerTests.WithContextSwitches.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 ﻿using System.Security.Claims;
 using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.TestUtils;
