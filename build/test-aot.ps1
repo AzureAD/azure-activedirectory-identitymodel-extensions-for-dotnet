ad64ace882fe58614d06db240b369a3a0f51c939
Saurabh Gautam <106561208+SaurabhMSFT@users.noreply.github.com>
2024-05-17 14:06:51 +0100

    Perform AOT check on repository (#2598)
    * Perform AOT check on repository
    
    * Use param
    
    * Use correct path
    
    * Use parameter
    
    * Update yml and ps file

---
+++ build/test-aot.ps1
@@ -0,0 +1,50 @@
+param([string]$targetNetFramework)
+
+$projectName='Microsoft.IdentityModel.AotCompatibility.TestApp'
+$rootDirectory = Split-Path $PSScriptRoot -Parent
+$publishOutput = dotnet publish $rootDirectory/test/$projectName/$projectName.csproj --self-contained -nodeReuse:false /p:UseSharedCompilation=false
+
+$actualWarningCount = 0
+
+foreach ($line in $($publishOutput -split "`r`n"))
+{
+    if (($line -like "*analysis warning IL*") -or ($line -like "*analysis error IL*"))
+    {
+        Write-Host $line
+        $actualWarningCount += 1
+    }
+}
+
+Write-Host "Actual warning count is: ", $actualWarningCount
+$expectedWarningCount = 0
+
+if ($LastExitCode -ne 0)
+{
+    Write-Host "There was an error while publishing AotCompatibility Test App. LastExitCode is:", $LastExitCode
+    Write-Host $publishOutput
+}
+
+$runtime = if ($IsWindows) {  "win-x64" } elseif ($IsMacOS) { "macos-x64"} else {"linux-x64"}
+$app = if ($IsWindows ) {"./$projectName.exe" } else {"./projectName" }
+
+Push-Location $rootDirectory/test/$projectName/bin/Release/$targetNetFramework/win-x64
+
+Write-Host "Executing test App..."
+$app
+Write-Host "Finished executing test App"
+
+if ($LastExitCode -ne 0)
+{
+  Write-Host "There was an error while executing AotCompatibility Test App. LastExitCode is:", $LastExitCode
+}
+
+Pop-Location
+
+$testPassed = 0
+if ($actualWarningCount -ne $expectedWarningCount)
+{
+    $testPassed = 1
+    Write-Host "Actual warning count:", actualWarningCount, "is not as expected. Expected warning count is:", $expectedWarningCount
+}
+
+Exit $testPassed
