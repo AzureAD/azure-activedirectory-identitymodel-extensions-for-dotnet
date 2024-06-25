a02a12b37e61732b240b5a5b25d180d3928b2f80
nguyencuong2596 <164532638+nguyencuong2596@users.noreply.github.com>
2024-06-25 19:32:53 +0100

    Add prelim implementation for default DistributedCache

---
+++ src/Microsoft.IdentityModel.Protocols/Configuration/DistributedCache.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.IdentityModel.Tokens.Json;
+
+namespace Microsoft.IdentityModel.Protocols.Configuration
+{
+    /// <summary>
+    /// </summary>
+    class DistributedCache : IDistributedCache
+    {
+        private readonly Dictionary<string, string> _storage = new();
+
+        public Task<string> GetStringAsync(string key, CancellationToken cancellationToken)
+        {
+            if (_storage.TryGetValue(key, out var value))
+            {
+                return Task.FromResult(value);
+            }
+
+            return Task.FromResult<string>(null);
+        }
+
+        public Task SetStringAsync(string key, string value, CancellationToken cancellationToken)
+        {
+            _storage[key] = value;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Microsoft.IdentityModel.Protocols/Configuration/IDistributedCache.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.IdentityModel.Protocols.Configuration
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IDistributedCache
+    {
+        /// <summary>
+        /// Retrieves the string value associated with the specified key from the distributed cache asynchronously.
+        /// This operation is cancellable via the provided `CancellationToken`.
+        /// </summary>
+        /// <param name="key">Property key.</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        /// <returns>
+        /// The string value associated with the specified key. If the key does not exist in the cache, result is null.
+        /// </returns>
+        Task<string> GetStringAsync(string key, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Stores a string value with the specified key in the distributed cache asynchronously.
+        /// This operation is cancellable via the provided `CancellationToken`.
+        /// </summary>
+        /// <param name="key">Property key.</param>
+        /// <param name="value">Property value.</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        Task SetStringAsync(string key, string value, CancellationToken cancellationToken);
+    }
+}
