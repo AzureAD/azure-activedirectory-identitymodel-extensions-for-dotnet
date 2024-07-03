bc0b057aa1daa81bd307844c9e6cd839a129a852
Franco Fung <francofung@microsoft.com>
2024-07-02 23:41:49 -0700

    Clean-up

--- src/Microsoft.IdentityModel.Tokens/Validation/Validators.TokenType.cs
+++ src/Microsoft.IdentityModel.Tokens/Validation/Validators.TokenType.cs
@@ -60,6 +60,18 @@ namespace Microsoft.IdentityModel.Tokens
             return type;
         }
 
+        /// <summary>
+        /// Validates the type of the token.
+        /// </summary>
+        /// <param name="type">The token type or <c>null</c> if it couldn't be resolved (e.g from the 'typ' header for a JWT).</param>
+        /// <param name="securityToken">The <see cref="SecurityToken"/> that is being validated.</param>
+        /// <param name="validationParameters"><see cref="TokenValidationParameters"/> required for validation.</param>
+        /// <param name="callContext"></param>
+        /// <exception cref="ArgumentNullException">If <paramref name="validationParameters"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">If <paramref name="securityToken"/> is null.</exception>
+        /// <exception cref="SecurityTokenInvalidTypeException">If <paramref name="type"/> is null or whitespace and <see cref="TokenValidationParameters.ValidTypes"/> is not null.</exception>
+        /// <exception cref="SecurityTokenInvalidTypeException">If <paramref name="type"/> failed to match <see cref="TokenValidationParameters.ValidTypes"/>.</exception>
+        /// <remarks>An EXACT match is required. <see cref="StringComparison.Ordinal"/> (case sensitive) is used for comparing <paramref name="type"/> against <see cref="TokenValidationParameters.ValidTypes"/>.</remarks>
 #pragma warning disable CA1801 // TODO: remove pragma disable once callContext is used for logging
         internal static TokenTypeValidationResult ValidateTokenType(string? type, SecurityToken? securityToken, TokenValidationParameters validationParameters, CallContext callContext)
 #pragma warning restore CA1801 // TODO: remove pragma disable once callContext is used for logging
@@ -99,16 +111,18 @@ namespace Microsoft.IdentityModel.Tokens
                 return ValidateTokenTypeUsingDelegate(type, securityToken, validationParameters);
             }
 
+            // Note: don't return an invalid TokenTypeValidationResult for a null or empty token type when a user-defined delegate is set
+            // to allow it to extract the actual token type from a different location (e.g from the claims).
             if (string.IsNullOrEmpty(type))
             {
                 return new TokenTypeValidationResult(
                     type,
-                    ValidationFailureType.NullArgument,
+                    ValidationFailureType.TokenTypeValidationFailed,
                     new ExceptionDetail(
                         new MessageDetail(
                             LogMessages.IDX10256,
                             LogHelper.MarkAsNonPII(nameof(type))),
-                        typeof(ArgumentNullException),
+                        typeof(SecurityTokenInvalidTypeException),
                         new StackFrame(true)));
             }
 
