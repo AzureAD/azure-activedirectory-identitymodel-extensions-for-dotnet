e07f4cc55cb8e7404363826ac133d152b7cd28dc
nguyencuong2596 <164532638+nguyencuong2596@users.noreply.github.com>
2024-07-05 16:28:13 +0100

    Add Distributed Components

---
+++ src/Microsoft.IdentityModel.Protocols/Configuration/DistributedConfigurationManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Microsoft.IdentityModel.Protocols.Configuration
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DistributedConfigurationManager<T> :
+        BaseConfigurationManager,
+        IConfigurationManager<T>,
+        IDistributedConfigurationManager<T> where T : class
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cancel"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<T> GetConfigurationAsync(CancellationToken cancel) => throw new NotImplementedException();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public override void RequestRefresh() => throw new NotImplementedException();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="metadataAddress"></param>
+        /// <param name="distributedConfigurationOptions"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        Task<T> IDistributedConfigurationManager<T>.GetConfigurationAsync(
+            string metadataAddress,
+            DistributedConfigurationOptions distributedConfigurationOptions,
+            CancellationToken cancellationToken) => throw new NotImplementedException();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="metadataAddress"></param>
+        /// <param name="configuration"></param>
+        /// <param name="distributedConfigurationOptions"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        Task IDistributedConfigurationManager<T>.SetConfigurationAsync(
+            string metadataAddress,
+            T configuration,
+            DistributedConfigurationOptions distributedConfigurationOptions,
+            CancellationToken cancellationToken) => throw new NotImplementedException();
+    }
+}
---
+++ src/Microsoft.IdentityModel.Protocols/IDistributedConfigurationManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.IdentityModel.Protocols
+{
+    internal interface IDistributedConfigurationManager<T> where T : class
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="metadataAddress"></param>
+        /// <param name="distributedConfigurationOptions"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<T> GetConfigurationAsync(
+            string metadataAddress,
+            DistributedConfigurationOptions distributedConfigurationOptions,
+            CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="metadataAddress"></param>
+        /// <param name="configuration"></param>
+        /// <param name="distributedConfigurationOptions"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task SetConfigurationAsync(
+            string metadataAddress,
+            T configuration,
+            DistributedConfigurationOptions distributedConfigurationOptions,
+            CancellationToken cancellationToken = default);
+    }
+}
