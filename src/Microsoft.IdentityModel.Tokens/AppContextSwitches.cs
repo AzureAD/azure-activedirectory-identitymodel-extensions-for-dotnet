98fc5f445aace03cc0124283639c446bb38efda2
Peter <34331512+pmaytak@users.noreply.github.com>
2024-07-11 15:20:21 -0700

    Update AppContextSwitches

--- src/Microsoft.IdentityModel.Tokens/AppContextSwitches.cs
+++ src/Microsoft.IdentityModel.Tokens/AppContextSwitches.cs
@@ -2,14 +2,18 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Security.Claims;
 
 namespace Microsoft.IdentityModel.Tokens
 {
     /// <summary>
     /// AppContext switches for Microsoft.IdentityModel.Tokens and referencing packages.
     /// </summary>
-    public static class AppContextSwitches
+    internal static class AppContextSwitches
     {
+        /// <summary>
+        /// Enables a fallback to the previous behavior of using <see cref="ClaimsIdentity"/> instead of <see cref="CaseSensitiveClaimsIdentity"/> globally.
+        /// </summary>
         internal const string UseClaimsIdentityTypeSwitch = "Microsoft.IdentityModel.Tokens.UseClaimsIdentityType";
 
         internal static bool UseClaimsIdentityType() => (AppContext.TryGetSwitch(UseClaimsIdentityTypeSwitch, out bool useClaimsIdentityType) && useClaimsIdentityType);
