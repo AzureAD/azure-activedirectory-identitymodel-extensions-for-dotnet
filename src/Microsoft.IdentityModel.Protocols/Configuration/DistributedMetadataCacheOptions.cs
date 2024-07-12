7b70599f94e71ab0edc3bc4836ca0fceefd6c934
Westin Musser <westinmusser@microsoft.com>
2024-07-12 16:27:06 -0700

    Create interface for dist metadata cache options

---
+++ src/Microsoft.IdentityModel.Protocols/Configuration/DistributedMetadataCacheOptions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Microsoft.IdentityModel.Protocols.Configuration
+{
+    /// <summary>
+    /// Specifies the DistributedMetadataCacheOptions which can be used to configure the distributed metadata cache.
+    /// </summary>
+    public class DistributedMetadataCacheOptions
+    {
+        /// <inheritdoc/>
+        public IDistributedCache DistributedCache { get; set; }
+
+        /// <inheritdoc/>
+        public IByteArrayCacheTransformer ByteArrayCacheTransformer { get; set; }
+    }
+}
---
+++ src/Microsoft.IdentityModel.Protocols/Configuration/IByteArrayCacheTransformer.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.IdentityModel.Protocols.Configuration
+{
+    /// <summary>
+    /// Interface that defines methods to transform byte arrays before they are written to a cache and after they are retrieved from a cache.
+    /// </summary>
+    public interface IByteArrayCacheTransformer
+    {
+        /// <summary>
+        /// Transform the provided <see cref="byte"/> array before it is written to a cache.
+        /// </summary>
+        /// <param name="data">The <see cref="byte"/>s to be transformed.</param>
+        /// <returns>The result of the transformation.</returns>
+        byte[] TransformBeforeCaching(byte[] data);
+
+        /// <summary>
+        /// TRansform the provided <see cref="byte"/> array after it is retrieved from a cache.
+        /// </summary>
+        /// <param name="data">The <see cref="byte"/>s to be transformed.</param>
+        /// <returns>The result of the transformation.</returns>
+        byte[] TransformAfterRetrieval(byte[] data);
+    }
+}
