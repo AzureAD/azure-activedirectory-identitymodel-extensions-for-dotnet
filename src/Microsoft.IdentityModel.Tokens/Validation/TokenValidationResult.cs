ae67205298a764e2b97c7215d29d163fd4089042
id4s <user@contoso.com>
2024-07-15 16:42:57 -0700

    picked up SAML changes and TokenValidationResult

--- src/Microsoft.IdentityModel.Tokens/Validation/TokenValidationResult.cs
+++ src/Microsoft.IdentityModel.Tokens/Validation/TokenValidationResult.cs
@@ -135,7 +135,7 @@ namespace Microsoft.IdentityModel.Tokens
 
                     if (_validationParameters != null && SecurityToken != null && _tokenHandler != null && Issuer != null)
                     {
-                        _claimsIdentity = ClaimsIdentityFactory.Create(_tokenHandler, SecurityToken, _validationParameters, Issuer);
+                        _claimsIdentity = _tokenHandler.CreateClaimsIdentityInternal(SecurityToken, _validationParameters, Issuer);
                     }
 
                     _claimsIdentityInitialized = true;
