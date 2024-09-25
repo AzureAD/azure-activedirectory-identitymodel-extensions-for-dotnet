06b8633c440beaf146d4453859e8efb8a546b3e4
Ignacio Inglese <iinglese@microsoft.com>
2024-09-25 16:07:44 +0100

    Added extra checks to the comparison between results. Added method to compare custom properties in SecurityTokenException derived classes.

--- test/Microsoft.IdentityModel.JsonWebTokens.Tests/JsonWebTokenHandler.ValidateTokenAsyncTests.Common.cs
+++ test/Microsoft.IdentityModel.JsonWebTokens.Tests/JsonWebTokenHandler.ValidateTokenAsyncTests.Common.cs
@@ -17,12 +17,17 @@ namespace Microsoft.IdentityModel.JsonWebTokens.Tests
             CompareContext context)
         {
             JsonWebTokenHandler jsonWebTokenHandler = new JsonWebTokenHandler();
+
+            // Validate the token using TokenValidationParameters
             TokenValidationResult legacyTokenValidationParametersResult =
                 await jsonWebTokenHandler.ValidateTokenAsync(jwtString, theoryData.TokenValidationParameters);
+
+            // Validate the token using ValidationParameters
             ValidationResult<ValidatedToken> validationParametersResult =
                 await jsonWebTokenHandler.ValidateTokenAsync(
                     jwtString, theoryData.ValidationParameters!, theoryData.CallContext, CancellationToken.None);
 
+            // Ensure the validity of the results match the expected result
             if (legacyTokenValidationParametersResult.IsValid != theoryData.ExpectedIsValid)
                 context.AddDiff($"tokenValidationParametersResult.IsValid != theoryData.ExpectedIsValid");
 
@@ -33,6 +38,7 @@ namespace Microsoft.IdentityModel.JsonWebTokens.Tests
                 legacyTokenValidationParametersResult.IsValid &&
                 validationParametersResult.IsSuccess)
             {
+                // Compare the ClaimsPrincipal and ClaimsIdentity from one result against the other
                 IdentityComparer.AreEqual(
                     legacyTokenValidationParametersResult.ClaimsIdentity,
                     validationParametersResult.UnwrapResult().ClaimsIdentity,
@@ -44,17 +50,44 @@ namespace Microsoft.IdentityModel.JsonWebTokens.Tests
             }
             else
             {
+                // Verify the exception provided by the TokenValidationParameters path
                 theoryData.ExpectedException.ProcessException(legacyTokenValidationParametersResult.Exception, context);
 
                 if (!validationParametersResult.IsSuccess)
                 {
-                    // If there is a special case for the ValidationParameters path, use that.
+                    // Verify the exception provided by the ValidationParameters path
                     if (theoryData.ExpectedExceptionValidationParameters is not null)
+                    {
+                        // If there is a special case for the ValidationParameters path, use that.
                         theoryData.ExpectedExceptionValidationParameters
                             .ProcessException(validationParametersResult.UnwrapError().GetException(), context);
+                    }
                     else
+                    {
                         theoryData.ExpectedException
                             .ProcessException(validationParametersResult.UnwrapError().GetException(), context);
+
+                        // If the expected exception is the same in both paths, verify the message matches
+                        IdentityComparer.AreStringsEqual(
+                            legacyTokenValidationParametersResult.Exception.Message,
+                            validationParametersResult.UnwrapError().GetException().Message,
+                            context);
+                    }
+                }
+
+                // Verify that the exceptions are of the same type.
+                IdentityComparer.AreEqual(
+                    legacyTokenValidationParametersResult.Exception.GetType(),
+                    validationParametersResult.UnwrapError().GetException().GetType(),
+                    context);
+
+                if (legacyTokenValidationParametersResult.Exception is SecurityTokenException)
+                {
+                    // Verify that the custom properties are the same.
+                    IdentityComparer.AreSecurityTokenExceptionsEqual(
+                        legacyTokenValidationParametersResult.Exception,
+                        validationParametersResult.UnwrapError().GetException(),
+                        context);
                 }
             }
         }
