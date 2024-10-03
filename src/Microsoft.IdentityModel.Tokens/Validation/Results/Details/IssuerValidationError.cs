0c4f23618c3e808bf0d88904f641c767edd01a71
Ignacio Inglese <iinglese@microsoft.com>
2024-10-03 15:55:45 +0100

    Added custom ValidationError class for issuer errors. Updated IssuerValidationDelegateAsync to use it.

---
+++ src/Microsoft.IdentityModel.Tokens/Validation/Results/Details/IssuerValidationError.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+
+#nullable enable
+namespace Microsoft.IdentityModel.Tokens
+{
+    internal class IssuerValidationError : ValidationError
+    {
+        private string? _invalidIssuer;
+
+        public IssuerValidationError(
+            MessageDetail messageDetail,
+            Type exceptionType,
+            StackFrame stackFrame,
+            string? invalidIssuer)
+            : base(messageDetail, ValidationFailureType.IssuerValidationFailed, exceptionType, stackFrame)
+        {
+            _invalidIssuer = invalidIssuer;
+        }
+
+        internal override void AddAdditionalInformation(ISecurityTokenException exception)
+        {
+            if (exception is SecurityTokenInvalidIssuerException invalidIssuerException)
+                invalidIssuerException.InvalidIssuer = _invalidIssuer;
+        }
+    }
+}
+#nullable restore
