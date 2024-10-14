973a9ffff182354581ee3ad8f17546b8bcbc55c4
Westin Musser <westinmusser@microsoft.com>
2024-10-14 15:55:20 -0700

    mark APIs as shipped where applicable edit script to also mark internal APIs

--- Tools/mark-shipped.ps1
+++ Tools/mark-shipped.ps1
@@ -4,16 +4,18 @@ param ()
 Set-StrictMode -version 2.0
 $ErrorActionPreference = "Stop"
 
-function MarkShipped([string]$dir) {
-    $shippedFilePath = Join-Path $dir "PublicAPI.Shipped.txt"
+function MarkShipped([string]$dir, [string]$access) {
+    $shippedFileName = $access + "API.Shipped.txt"
+    $shippedFilePath = Join-Path $dir $shippedFileName
     $shipped = @()
     $shipped += Get-Content $shippedFilePath
 
-    $unshippedFilePath = Join-Path $dir "PublicAPI.Unshipped.txt"
+    $unshippedFileName = $access + "API.Unshipped.txt"
+    $unshippedFilePath = Join-Path $dir $unshippedFileName
     $unshipped = Get-Content $unshippedFilePath
     $removed = @()
     $removedPrefix = "*REMOVED*";
-    Write-Host "Processing $dir"
+    Write-Host "Processing $dir : $access"
 
     foreach ($item in $unshipped) {
         if ($item.Length -gt 0) {
@@ -34,7 +36,12 @@ function MarkShipped([string]$dir) {
 try {
     foreach ($file in Get-ChildItem -re -in "PublicApi.Shipped.txt") {
         $dir = Split-Path -parent $file
-        MarkShipped $dir
+        MarkShipped $dir "Public"
+    }
+
+    foreach ($file in Get-ChildItem -re -in "InternalApi.Shipped.txt") {
+        $dir = Split-Path -parent $file
+        MarkShipped $dir "Internal"
     }
 }
 catch {
