b9c0d35eac2e2518e64393d19a2a6194b269498d
Ignacio Inglese <iinglese@microsoft.com>
2024-11-22 09:34:17 +0000

    Update src/Microsoft.IdentityModel.Tokens/Validation/ValidationFailureType.cs
    Co-authored-by: Westin Musser <127992899+westin-m@users.noreply.github.com>

--- src/Microsoft.IdentityModel.Tokens/Validation/ValidationFailureType.cs
+++ src/Microsoft.IdentityModel.Tokens/Validation/ValidationFailureType.cs
@@ -136,7 +136,7 @@ namespace Microsoft.IdentityModel.Tokens
         private class IssuerValidatorFailure : ValidationFailureType { internal IssuerValidatorFailure(string name) : base(name) { } }
 
         /// <summary>
-        /// Defines a type that represents that the token type validation delegate threw and exception.
+        /// Defines a type that represents the fact that the token type validation delegate threw an exception.
         /// </summary>
         public static readonly ValidationFailureType TokenTypeValidatorThrew = new TokenTypeValidationFailure("TokenTypeValidatorThrew");
     }
