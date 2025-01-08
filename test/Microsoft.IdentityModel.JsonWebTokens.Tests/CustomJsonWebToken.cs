301a40e9cb419e7e98676b32bad0e15ec25fb85f
Peter <34331512+pmaytak@users.noreply.github.com>
2025-01-08 00:25:16 -0800

    Test updates.

--- test/Microsoft.IdentityModel.JsonWebTokens.Tests/CustomJsonWebToken.cs
+++ test/Microsoft.IdentityModel.JsonWebTokens.Tests/CustomJsonWebToken.cs
@@ -1,13 +1,27 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 using System.Collections.Generic;
 using System.Text.Json;
-using Microsoft.IdentityModel.Tokens.Json;
 
 namespace Microsoft.IdentityModel.JsonWebTokens.Tests
 {
     public class CustomJsonWebToken : JsonWebToken
     {
-        private const string CustomClaimName = "CustomClaim";
+        // Represents claims known to this custom implementation and not to the IdentityModel.
+        public const string CustomClaimName = "CustomClaim";
+
+        private CustomClaim _customClaim;
+
+        public CustomClaim CustomClaim
+        {
+            get
+            {
+                _customClaim ??= Payload.GetValue<CustomClaim>(CustomClaimName);
+                return _customClaim;
+            }
+        }
 
         public CustomJsonWebToken(string jwtEncodedString) : base(jwtEncodedString) { }
 
@@ -17,26 +31,29 @@ namespace Microsoft.IdentityModel.JsonWebTokens.Tests
 
         private protected override void ReadPayloadValue(ref Utf8JsonReader reader, IDictionary<string, object> claims)
         {
+            // Handle custom claims.
             if (reader.ValueTextEquals(CustomClaimName))
             {
-                _customClaim = JsonSerializerPrimitives.ReadString(ref reader, CustomClaimName, ClassName, true);
+                // Deserialize the custom object claim in an appropriate way.
+                reader.Read(); // Move to the value.
+                _customClaim = JsonSerializer.Deserialize<CustomClaim>(reader.GetString());
                 claims[CustomClaimName] = _customClaim;
+                reader.Read();
             }
             else
             {
+                // Call base implementation to handle other claims known to IdentityModel.
                 base.ReadPayloadValue(ref reader, claims);
             }
         }
+    }
 
-        private string _customClaim;
-
-        public string CustomClaim
+    public class CustomClaim
+    {
+        public CustomClaim()
         {
-            get
-            {
-                _customClaim ??= Payload.GetStringValue(CustomClaimName);
-                return _customClaim;
-            }
         }
+
+        public string CustomClaimValue { get; set; }
     }
 }
