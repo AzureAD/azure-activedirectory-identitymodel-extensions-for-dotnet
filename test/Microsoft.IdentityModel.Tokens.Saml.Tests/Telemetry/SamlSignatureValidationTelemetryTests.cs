060500b12460629137397f85998c19e92b79ff02
Ignacio Inglese <iinglese@microsoft.com>
2026-01-12 20:32:50 +0000

    Added tests for SAML and SAML2 signature validation telemetry

---
+++ test/Microsoft.IdentityModel.Tokens.Saml.Tests/Telemetry/SamlSignatureValidationTelemetryTests.cs
@@ -0,0 +1,421 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Telemetry;
+using Microsoft.IdentityModel.TestUtils;
+using Xunit;
+
+namespace Microsoft.IdentityModel.Tokens.Saml.Tests.Telemetry;
+
+public class SamlSignatureValidationTelemetryTests
+{
+    [Fact]
+    public void ValidateToken_SamlSignatureValidationSuccess_RecordsTelemetry()
+    {
+        // Arrange
+        var listener = new TestMeterListener();
+        ITestingTokenHandler handler = new SamlSecurityTestingTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = Default.ClaimsIdentityLongNames,
+            SigningCredentials = Default.AsymmetricSigningCredentials,
+            Audience = Default.Audience,
+            Expires = DateTime.UtcNow + TimeSpan.FromDays(1),
+            Issuer = Default.Issuer
+        };
+
+        var tokenString = handler.CreateStringToken(tokenDescriptor);
+
+        var validationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = Default.AsymmetricSigningCredentials.Key,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateLifetime = false,
+        };
+
+        // Act
+        handler.ValidateTokenAsync(tokenString, validationParameters);
+
+        // Assert
+        AssertTelemetryRecorded(listener, TelemetryDataRecorder.SignatureValidationCounterName,
+            new Dictionary<string, object>
+            {
+                { "status", "success" },
+                { "alg", Default.AsymmetricSigningCredentials.Algorithm },
+                { "key_size", Default.AsymmetricSigningCredentials.Key.KeySize }
+            });
+    }
+
+    [Fact]
+    public void ValidateToken_SamlSignatureValidationFailure_RecordsTelemetry()
+    {
+        // Arrange
+        var listener = new TestMeterListener();
+        ITestingTokenHandler handler = new SamlSecurityTestingTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = Default.ClaimsIdentityLongNames,
+            SigningCredentials = KeyingMaterial.DefaultX509SigningCreds_2048_RsaSha2_Sha2,
+            Audience = Default.Audience,
+            Expires = DateTime.UtcNow + TimeSpan.FromDays(1),
+            Issuer = Default.Issuer
+        };
+
+        var tokenString = handler.CreateStringToken(tokenDescriptor);
+
+        // Use a different key for validation (should fail)
+        var validationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = KeyingMaterial.RsaSecurityKey2,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateLifetime = false,
+        };
+
+        // Act
+        try
+        {
+            handler.ValidateTokenAsync(tokenString, validationParameters);
+        }
+        catch
+        {
+            // Expected to throw
+        }
+
+        // Assert
+        AssertTelemetryRecorded(listener, TelemetryDataRecorder.SignatureValidationCounterName,
+            new Dictionary<string, object>
+            {
+                { "status", "failure" },
+                { "alg", KeyingMaterial.DefaultX509SigningCreds_2048_RsaSha2_Sha2.Algorithm },
+                { "key_size", KeyingMaterial.RsaSecurityKey2.KeySize }
+            });
+    }
+
+    [Fact]
+    public void ValidateToken_Saml2SignatureValidationSuccess_RecordsTelemetry()
+    {
+        // Arrange
+        var listener = new TestMeterListener();
+        ITestingTokenHandler handler = new Saml2SecurityTestingTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = Default.ClaimsIdentityLongNames,
+            SigningCredentials = Default.AsymmetricSigningCredentials,
+            Audience = Default.Audience,
+            Expires = DateTime.UtcNow + TimeSpan.FromDays(1),
+            Issuer = Default.Issuer
+        };
+
+        var tokenString = handler.CreateStringToken(tokenDescriptor);
+
+        var validationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = Default.AsymmetricSigningCredentials.Key,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateLifetime = false,
+        };
+
+        // Act
+        handler.ValidateTokenAsync(tokenString, validationParameters);
+
+        // Assert
+        AssertTelemetryRecorded(listener, TelemetryDataRecorder.SignatureValidationCounterName,
+            new Dictionary<string, object>
+            {
+                { "status", "success" },
+                { "alg", Default.AsymmetricSigningCredentials.Algorithm },
+                { "key_size", Default.AsymmetricSigningCredentials.Key.KeySize }
+            });
+    }
+
+    [Fact]
+    public void ValidateToken_Saml2SignatureValidationFailure_RecordsTelemetry()
+    {
+        // Arrange
+        var listener = new TestMeterListener();
+        ITestingTokenHandler handler = new Saml2SecurityTestingTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = Default.ClaimsIdentityLongNames,
+            SigningCredentials = KeyingMaterial.DefaultX509SigningCreds_2048_RsaSha2_Sha2,
+            Audience = Default.Audience,
+            Expires = DateTime.UtcNow + TimeSpan.FromDays(1),
+            Issuer = Default.Issuer
+        };
+
+        var tokenString = handler.CreateStringToken(tokenDescriptor);
+
+        // Use a different key for validation (should fail)
+        var validationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = KeyingMaterial.RsaSecurityKey2,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateLifetime = false,
+        };
+
+        // Act
+        try
+        {
+            handler.ValidateTokenAsync(tokenString, validationParameters);
+        }
+        catch
+        {
+            // Expected to throw
+        }
+
+        // Assert
+        AssertTelemetryRecorded(listener, TelemetryDataRecorder.SignatureValidationCounterName,
+            new Dictionary<string, object>
+            {
+                { "status", "failure" },
+                { "alg", KeyingMaterial.DefaultX509SigningCreds_2048_RsaSha2_Sha2.Algorithm },
+                { "key_size", KeyingMaterial.RsaSecurityKey2.KeySize }
+            });
+    }
+
+    [Fact]
+    public void ValidateToken_SamlSymmetricSignature_RecordsTelemetry()
+    {
+        // Arrange
+        var listener = new TestMeterListener();
+        ITestingTokenHandler handler = new SamlSecurityTestingTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = Default.ClaimsIdentityLongNames,
+            SigningCredentials = Default.SymmetricSigningCredentials,
+            Audience = Default.Audience,
+            Expires = DateTime.UtcNow + TimeSpan.FromDays(1),
+            Issuer = Default.Issuer
+        };
+
+        var tokenString = handler.CreateStringToken(tokenDescriptor);
+
+        var validationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = Default.SymmetricSigningCredentials.Key,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateLifetime = false,
+        };
+
+        // Act
+        handler.ValidateTokenAsync(tokenString, validationParameters);
+
+        // Assert
+        AssertTelemetryRecorded(listener, TelemetryDataRecorder.SignatureValidationCounterName,
+            new Dictionary<string, object>
+            {
+                { "status", "success" },
+                { "alg", Default.SymmetricSigningCredentials.Algorithm },
+                { "key_size", Default.SymmetricSigningCredentials.Key.KeySize }
+            });
+    }
+
+    [Fact]
+    public void ValidateToken_Saml2SymmetricSignature_RecordsTelemetry()
+    {
+        // Arrange
+        var listener = new TestMeterListener();
+        ITestingTokenHandler handler = new Saml2SecurityTestingTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = Default.ClaimsIdentityLongNames,
+            SigningCredentials = Default.SymmetricSigningCredentials,
+            Audience = Default.Audience,
+            Expires = DateTime.UtcNow + TimeSpan.FromDays(1),
+            Issuer = Default.Issuer
+        };
+
+        var tokenString = handler.CreateStringToken(tokenDescriptor);
+
+        var validationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = Default.SymmetricSigningCredentials.Key,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateLifetime = false,
+        };
+
+        // Act
+        handler.ValidateTokenAsync(tokenString, validationParameters);
+
+        // Assert
+        AssertTelemetryRecorded(listener, TelemetryDataRecorder.SignatureValidationCounterName,
+            new Dictionary<string, object>
+            {
+                { "status", "success" },
+                { "alg", Default.SymmetricSigningCredentials.Algorithm },
+                { "key_size", Default.SymmetricSigningCredentials.Key.KeySize }
+            });
+    }
+
+    [Fact]
+    public void ValidateToken_SamlRsa2048Signature_RecordsTelemetry()
+    {
+        // Arrange
+        var listener = new TestMeterListener();
+        ITestingTokenHandler handler = new SamlSecurityTestingTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = Default.ClaimsIdentityLongNames,
+            SigningCredentials = KeyingMaterial.DefaultX509SigningCreds_2048_RsaSha2_Sha2,
+            Audience = Default.Audience,
+            Expires = DateTime.UtcNow + TimeSpan.FromDays(1),
+            Issuer = Default.Issuer
+        };
+
+        var tokenString = handler.CreateStringToken(tokenDescriptor);
+
+        var validationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = KeyingMaterial.DefaultX509SigningCreds_2048_RsaSha2_Sha2.Key,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateLifetime = false,
+        };
+
+        // Act
+        handler.ValidateTokenAsync(tokenString, validationParameters);
+
+        // Assert
+        AssertTelemetryRecorded(listener, TelemetryDataRecorder.SignatureValidationCounterName,
+            new Dictionary<string, object>
+            {
+                { "status", "success" },
+                { "alg", KeyingMaterial.DefaultX509SigningCreds_2048_RsaSha2_Sha2.Algorithm },
+                { "key_size", 2048 }
+            });
+    }
+
+    [Fact]
+    public void ValidateToken_Saml2Rsa2048Signature_RecordsTelemetry()
+    {
+        // Arrange
+        var listener = new TestMeterListener();
+        ITestingTokenHandler handler = new Saml2SecurityTestingTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = Default.ClaimsIdentityLongNames,
+            SigningCredentials = KeyingMaterial.DefaultX509SigningCreds_2048_RsaSha2_Sha2,
+            Audience = Default.Audience,
+            Expires = DateTime.UtcNow + TimeSpan.FromDays(1),
+            Issuer = Default.Issuer
+        };
+
+        var tokenString = handler.CreateStringToken(tokenDescriptor);
+
+        var validationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = KeyingMaterial.DefaultX509SigningCreds_2048_RsaSha2_Sha2.Key,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateLifetime = false,
+        };
+
+        // Act
+        handler.ValidateTokenAsync(tokenString, validationParameters);
+
+        // Assert
+        AssertTelemetryRecorded(listener, TelemetryDataRecorder.SignatureValidationCounterName,
+            new Dictionary<string, object>
+            {
+                { "status", "success" },
+                { "alg", KeyingMaterial.DefaultX509SigningCreds_2048_RsaSha2_Sha2.Algorithm },
+                { "key_size", 2048 }
+            });
+    }
+
+    private void AssertTelemetryRecorded(TestMeterListener listener, string counterName, Dictionary<string, object> expectedTags)
+    {
+        var measurements = listener.GetMeasurements(counterName);
+        Assert.NotEmpty(measurements);
+
+        var lastMeasurement = measurements.Last();
+        foreach (var expectedTag in expectedTags)
+        {
+            var actualTag = lastMeasurement.Tags.FirstOrDefault(t => t.Key == expectedTag.Key);
+            Assert.NotNull(actualTag.Value);
+            Assert.Equal(expectedTag.Value.ToString(), actualTag.Value.ToString());
+        }
+    }
+
+    /// <summary>
+    /// Test meter listener to capture telemetry measurements
+    /// </summary>
+    private class TestMeterListener
+    {
+        private readonly MeterListener _listener;
+        private readonly List<Measurement> _measurements = new List<Measurement>();
+
+        public TestMeterListener()
+        {
+            _listener = new MeterListener();
+            _listener.InstrumentPublished = (instrument, listener) =>
+            {
+                if (instrument.Meter.Name == "MicrosoftIdentityModel_Meter")
+                {
+                    listener.EnableMeasurementEvents(instrument);
+                }
+            };
+
+            _listener.SetMeasurementEventCallback<long>((instrument, measurement, tags, state) =>
+            {
+                lock (_measurements)
+                {
+                    _measurements.Add(new Measurement
+                    {
+                        InstrumentName = instrument.Name,
+                        Value = measurement,
+                        Tags = tags.ToArray()
+                    });
+                }
+            });
+
+            _listener.Start();
+        }
+
+        public List<Measurement> GetMeasurements(string instrumentName)
+        {
+            lock (_measurements)
+            {
+                return _measurements.Where(m => m.InstrumentName == instrumentName).ToList();
+            }
+        }
+
+        public class Measurement
+        {
+            public string InstrumentName { get; set; }
+            public long Value { get; set; }
+            public KeyValuePair<string, object>[] Tags { get; set; }
+        }
+    }
+
+    /// <summary>
+    /// Creates an RSA instance with the specified key size.
+    /// On .NET Framework, uses RSACng for better support of larger key sizes (3072, 4096).
+    /// On other platforms, uses the default RSA.Create().
+    /// </summary>
+    private static RSA CreateRsa(int keySize)
+    {
+#if NET462 || NET472
+        // Use RSACng on .NET Framework for better support of 3072/4096 bit keys
+        var rsa = new System.Security.Cryptography.RSACng();
+        rsa.KeySize = keySize;
+        return rsa;
+#else
+        var rsa = RSA.Create();
+        rsa.KeySize = keySize;
+        return rsa;
+#endif
+    }
+
+}
